33d16a782b245550b9bdca3e9a734bb90986dc0e
GeorgeZarokanellos <geo.zarak@gmail.com>
2023-12-28 18:50:50 +0200

    App is now server over https for added security and for pwa requirements

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,62 @@
+//#region require statements
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+//#endregion
+
+//SSL certificate
+const private_key = fs.readFileSync(process.env.SSL_KEY_PATH, 'utf-8');
+const certificate = fs.readFileSync(process.env.SSL_CERT_PATH, 'utf-8');
+const credentials = {
+    key: private_key,
+    cert: certificate 
+}
+
+//routers
 const registration_router = require('./routes/registration_routes');
-// const trip_router = require('./routes/trip_router');
+const trip_router = require('./routes/trip_routes');
 
 app.use(express.static('./methods-public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-
-
+//#region middleware
 app.use('/api/v1', registration_router);
-// app.use('/api/v1/trips', trip_router);
 
-
+app.use('/api/v1', trip_router);
 
 app.get('/', (req,res) => {
     res.status(200).send('Home Page');
 })
 
+app.use((req,res,next) => {
+    if(!req.secure){
+        return res.redirect('https://' + req.headers.host + req.url);
+        //req.headers.host is the domain name(localhost:5000) and req.url is the path(/api/v1/registration)
+    }
+    next();
+});
+//#endregion
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+
+//create https server with express js
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(5000, () => {
+    console.log('HTTPS Server running on port 5000');  
+});
+
+//#region redirect to https server
+//Create http server to redirect to https server for users that type in http://localhost:3000
+const httpApp = express();
+httpApp.get('*', (req,res) => {
+    res.redirect('https://' + req.headers.host + req.url);
+});
+const httpServer = http.createServer(httpApp);
+httpServer.listen(3000, () => {
+    console.log('HTTP Server running on port 3000');
 })
+//#endregion
