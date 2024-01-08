71673c6c3fbeb0cd03cde2d276733705c6741105
GeorgeZarokanellos <geo.zarak@gmail.com>
2024-01-09 01:01:21 +0200

    changed the display trips function in order to display the names of the driver and the passengers as well as the names of the stops

--- backend/controllers/trip_controllers.js
+++ backend/controllers/trip_controllers.js
@@ -1,10 +1,45 @@
-// const db = require('../database/db_connect');
 const sequelize = require('../database/connect_to_db');
-const Trip = require('../models/trip');
+const {Trip, TripPassengers, TripStops, Stops, User, Driver} = require('../models/associations');
 
 const displayTrips = async (req,res) => {
     try {
-        const trips = await Trip.findAll();
+        const trips = await Trip.findAll({
+            include: [
+                {
+                    model: Driver,
+                    as: 'driver',
+                    include:[
+                        {
+                            model: User,
+                            as: 'user',
+                            attributes: ['firstName', 'lastName']
+                        }
+                    ]
+                },
+                {
+                    model: TripPassengers,
+                    as: 'tripPassengers',
+                    include: [
+                        {
+                            model: User,
+                            as: 'passenger',
+                            attributes: ['firstName', 'lastName']
+                        }
+                    ]
+                },
+                {
+                    model: TripStops,
+                    as: 'tripStops',
+                    include: [
+                        {
+                            model: Stops,
+                            as: 'stopLocation',
+                            attributes: ['stopLoc']
+                        }
+                    ]
+                }
+            ]
+        });
         res.status(200).send(trips);
     } catch (error) {
         res.status(500).send('Error retrieving trips:' + error);
