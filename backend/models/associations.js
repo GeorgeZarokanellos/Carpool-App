275471c9be45465ead92e4c30c40a175d4725475
GeorgeZarokanellos <geo.zarak@gmail.com>
2024-01-09 00:59:18 +0200

    made an associations file that creates the associations after the models have been created

---
+++ backend/models/associations.js
@@ -0,0 +1,32 @@
+const Trip = require('./trip');
+const Stops = require('./stop');
+const TripStops = require('./trip_stops');
+const TripPassengers = require('./trip_passengers');
+const User = require('./user');
+const Driver = require('./driver');
+
+
+
+TripPassengers.belongsTo(Trip, {foreignKey: 'tripId', as: 'trip'});
+TripPassengers.belongsTo(User, {foreignKey: 'passengerId', as: 'passenger'});
+
+TripStops.belongsTo(Trip, {foreignKey: 'tripId'});
+TripStops.belongsTo(Stops, {foreignKey: 'stopId', as: 'stopLocation'});
+
+Trip.hasMany(TripStops, {foreignKey: 'tripId', as: 'tripStops'});
+Trip.hasMany(TripPassengers, {foreignKey: 'tripId', as: 'tripPassengers'});
+
+Trip.belongsTo(Driver, {foreignKey: 'driverId', as: 'driver'});
+
+Driver.belongsTo(User, {foreignKey: 'driverId', as: 'user'});
+
+
+
+module.exports = {
+    Trip,
+    TripStops,
+    TripPassengers,
+    Stops,
+    User,
+    Driver
+}
