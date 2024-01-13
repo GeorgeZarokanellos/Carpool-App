2925340da6bd21c0c74c2ee88cbaae5b061c8064
GeorgeZarokanellos <geo.zarak@gmail.com>
2024-01-13 19:15:53 +0200

    Now all the add and remove functions take a trip object as an argument instead of its id

--- backend/controllers/trip_controller.js
+++ backend/controllers/trip_controller.js
@@ -1,6 +1,6 @@
 const {Trip, TripPassengers, TripStops, Stops, User, Driver} = require('../models/associations');
 
-const addPassengersToTrip = async(passengers, tripId) => {
+const addPassengersToTrip = async(passengers, Trip) => {
     if(passengers){
         const firstNames = passengers.map(passenger => passenger.firstName);    //returns an array of first names of the passengers to be added
         const lastNames = passengers.map(passenger => passenger.lastName);      //returns an array of last names of the passengers to be added
@@ -14,12 +14,12 @@ const addPassengersToTrip = async(passengers, tripId) => {
         const tripPassengersIds = tripPassengersNames.map(passenger => passenger.userId);   //returns an array of the users ids to be added as passengers
         const tripPassengersPromises = tripPassengersIds.map(passengerId => {  //returns an array of promises for each TripPassengers entry
             return TripPassengers.create({
-                tripId: tripId,
+                tripId: Trip.tripId,
                 passengerId: passengerId
             });
         });
         const newTripPassengers = await Promise.all(tripPassengersPromises);    //wait for all the promises to be resolved
-        const allAddOperationsSuccessful = newTripPassengers.every(result => result.success); //check if all the passengers were added to the trip
+        const allAddOperationsSuccessful = newTripPassengers.every(result => !result.isNewRecord); //check if all the passengers were added to the trip
 
         if(allAddOperationsSuccessful){
             Trip.passengers += tripPassengersPromises.length;
@@ -30,7 +30,7 @@ const addPassengersToTrip = async(passengers, tripId) => {
     }
 }
 
-const removePassengersFromTrip = async(passengers, tripId) => {
+const removePassengersFromTrip = async(passengers, Trip) => {
     if(passengers){
         const firstNames = passengers.map(passenger => passenger.firstName);    //returns an array of first names of the passengers to be removed
         const lastNames = passengers.map(passenger => passenger.lastName);      //returns an array of last names of the passengers to be removed
@@ -45,13 +45,13 @@ const removePassengersFromTrip = async(passengers, tripId) => {
         const tripPassengersPromises = tripPassengersIds.map(passengerId => {  //returns an array of promises for each TripPassengers entry
             return TripPassengers.destroy({
                 where: {
-                    tripId: tripId,
+                    tripId: Trip.tripId,
                     passengerId: passengerId
                 }
             });
         });
         const removedTripPassengers = await Promise.all(tripPassengersPromises);    //wait for all the promises to be resolved
-        const allRemoveOperationsSuccessful = await removedTripPassengers.every(result => result.success);
+        const allRemoveOperationsSuccessful = await removedTripPassengers.every(result => !result.isNewRecord);
         if(allRemoveOperationsSuccessful){
             Trip.passengers -= tripPassengersPromises.length;
             await Trip.save();  //save the updated trip 
@@ -61,7 +61,7 @@ const removePassengersFromTrip = async(passengers, tripId) => {
     }
 }
 
-const addStopsToTrip = async(stops, tripId) => {
+const addStopsToTrip = async(stops, Trip) => {
     if(stops){
         const stopRecords = await Stops.findAll({   //returns an array of stop objects
             where: {
@@ -73,13 +73,15 @@ const addStopsToTrip = async(stops, tripId) => {
     
         const tripStopsPromises = stopIds.map(stopId => {   //create an array of promises for each TripsStop entry
             return TripStops.create({
-                tripId: tripId,
+                tripId: Trip.tripId,
                 stopId: stopId
             });
         });
         //wait for all the promises to be resolved
         const newTripStops = await Promise.all(tripStopsPromises); 
-        const allAddOperationsSuccessful = newTripStops.every(result => result.success); 
+        // console.log(newTripStops);
+        const allAddOperationsSuccessful = newTripStops.every(result => !result.isNewRecord); 
+        // console.log(allAddOperationsSuccessful);
         if(allAddOperationsSuccessful){
             Trip.stops += tripStopsPromises.length;
             await Trip.save();  //save the updated trip
@@ -89,7 +91,7 @@ const addStopsToTrip = async(stops, tripId) => {
     }
 }
 
-const removeStopsFromTrip = async(stops, tripId) => {
+const removeStopsFromTrip = async(stops, Trip) => {
     if(stops){
         const stopRecords = await Stops.findAll({   //returns an array of stop objects
             where: {
@@ -102,14 +104,14 @@ const removeStopsFromTrip = async(stops, tripId) => {
         const tripStopsPromises = stopIds.map(stopId => {   //create an array of promises for each TripsStop entry
             return TripStops.destroy({
                 where: {
-                    tripId: tripId,
+                    tripId: Trip.tripId,
                     stopId: stopId
                 }
             });
         });
         //wait for all the promises to be resolved
         const removedTripStops = await Promise.all(tripStopsPromises);    
-        const allRemoveOperationsSuccessful = removedTripStops.every(result => result.success);
+        const allRemoveOperationsSuccessful = removedTripStops.every(result => !result.isNewRecord);
         if(allRemoveOperationsSuccessful){
             Trip.stops -= tripStopsPromises.length;
             await Trip.save();  //save the updated trip
@@ -190,30 +192,27 @@ const createTrip = async (req,res) => {
     const finalDriverId = currentUserIsDriver ? currentUserId : driverId;
 
     const stops = req.body.stops;
-    const numberOfStops = stops.length;
 
     const passengers = req.body.passengers;
-    const numberOfPassengers = passengers.length;
 
     const newTrip = await Trip.create({
         driverId: finalDriverId,
         tripCreatorId: userId,
         startLocation: startLocation,
-        stops: numberOfStops,
-        passengers: numberOfPassengers,
-        tripDate: Date.now(),
-        status: 'planning'
+        tripDate: "2024-01-17",
+        status: 'planning'  
     });
 
-    await addStopsToTrip(stops, newTrip.tripId);
-    await addPassengersToTrip(passengers, newTrip.tripId);
+    await addStopsToTrip(stops, newTrip);
+    await addPassengersToTrip(passengers, newTrip);
 
     res.status(200).send(newTrip);
 }
 
 const updateTrip = async (req,res) => {
+    // console.log(req.params.id);
     const tripId = req.params.id;  //take the value from the placeholder in the URL
-    const typeOfUser = req.user.role;
+    // const typeOfUser = req.user.role;
     const updateDetails = req.body; // {startLocation, tripDate}
 
     const addPassengers = req.body.addPassengers;
@@ -225,18 +224,18 @@ const updateTrip = async (req,res) => {
         const trip = await Trip.findByPk(tripId);
         if(!trip)
             throw new Error(`Trip with id ${tripId} not found!`);
-        if(req.user.userId !== trip.tripCreatorId){
+        if(req.userId !== trip.tripCreatorId){ //only the user that created the trip can update it 
             // if(addPassengers){
             //     await addPassengersToTrip(addPassengers, tripId);
             // }
             if(removePassengers){
-                await removePassengersFromTrip(removePassengers, tripId);
+                await removePassengersFromTrip(removePassengers, trip);
             }
             if(addStops){
-                await addStopsToTrip(addStops, tripId);
+                await addStopsToTrip(addStops, trip);
             }
             if(removeStops){
-                await removeStopsFromTrip(removeStops, tripId);
+                await removeStopsFromTrip(removeStops, trip);
             }
         }
         await trip.update(updateDetails);
