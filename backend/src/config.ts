058ae32a2a22ad3a260733b0a8b15ff9d0ad56eb
GeorgeZarokanellos <geo.zarak@gmail.com>
2024-02-14 16:52:30 +0200

    changed the condition to check if the process is undefined instead of null

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -14,28 +14,27 @@ interface Env{  // interface for environment variables
 }
 
 function getEnv(): Env{ // checks if environment variables are set
-    if(process.env.DB_NAME !== null ||
-        process.env.DB_USER !== null ||
-        process.env.DB_PASSWORD !== null ||
-        process.env.DB_HOST !== null ||
-        process.env.DB_PORT !== null ||
-        process.env.SESSION_SECRET !== null ||
-        process.env.SSL_KEY_PATH !== null ||
-        process.env.SSL_CERT_PATH !== null){
+    if(process.env.DB_NAME === undefined  ||
+        process.env.DB_USER === undefined ||
+        process.env.DB_PASSWORD === undefined ||
+        process.env.DB_HOST === undefined ||
+        process.env.DB_PORT === undefined ||
+        process.env.SESSION_SECRET === undefined ||
+        process.env.SSL_KEY_PATH === undefined ||
+        process.env.SSL_CERT_PATH === undefined){
             throw new Error('Environment variables not set');
+    } else {
+        return {    // returns an object with the environment variables
+            DB_NAME: process.env.DB_NAME,
+            DB_USER: process.env.DB_USER,
+            DB_PASSWORD: process.env.DB_PASSWORD,
+            DB_HOST: process.env.DB_HOST,
+            DB_PORT: parseInt(process.env.DB_PORT),
+            SESSION_SECRET: process.env.SESSION_SECRET,
+            SSL_KEY_PATH: process.env.SSL_KEY_PATH,
+            SSL_CERT_PATH: process.env.SSL_CERT_PATH
         }
-
-    return {    // returns an object with the environment variables
-        DB_NAME: process.env.DB_NAME,
-        DB_USER: process.env.DB_USER,
-        DB_PASSWORD: process.env.DB_PASSWORD,
-        DB_HOST: process.env.DB_HOST,
-        DB_PORT: parseInt(process.env.DB_PORT),
-        SESSION_SECRET: process.env.SESSION_SECRET,
-        SSL_KEY_PATH: process.env.SSL_KEY_PATH,
-        SSL_CERT_PATH: process.env.SSL_CERT_PATH
-    }
-        
+    }        
 }
 
 export const env = getEnv();
