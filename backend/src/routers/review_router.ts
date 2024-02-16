5a6e73490e53ecb4dd057c794cb2062d33584a6d
GeorgeZarokanellos <geo.zarak@gmail.com>
2024-02-16 19:19:30 +0200

    update and delete review functionalities added to controller and router

--- backend/src/routers/review_router.ts
+++ backend/src/routers/review_router.ts
@@ -1,4 +1,5 @@
-import { getReviews } from '../controllers/reviews_controller';
+// import { check, validationResult } from 'express-validator';
+import { getReviews, createReview, updateReview, deleteReview } from '../controllers/reviews_controller';
 import express from 'express';
 
 const router = express.Router();
@@ -6,5 +7,7 @@ const router = express.Router();
 
 router.get('/:id', getReviews);
 router.post('/:reviewedPersonId', createReview);
+router.patch('/reviews/:id', updateReview);
+router.delete('/reviews/:id', deleteReview);
 
 export default router;
