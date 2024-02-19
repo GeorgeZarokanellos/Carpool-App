6ec49d7d5bce30a4a5738991b0be09e9a59ea1e3
GeorgeZarokanellos <geo.zarak@gmail.com>
2024-02-19 22:13:37 +0200

    added validate function to handle errors during validation

--- backend/src/utils/common_functions.ts
+++ backend/src/utils/common_functions.ts
@@ -1,5 +1,6 @@
 import { User, Review } from '../models/associations';
-
+import {validationResult} from 'express-validator';
+import type {Request, Response, NextFunction} from 'express';
 
 export const retrieveUserReviews = async (userId: string) : Promise<Review[]> => {
     // console.log(user);
@@ -30,6 +31,16 @@ export const retrieveUserReviews = async (userId: string) : Promise<Review[]> =>
     }
 };
 
+export const validate = (req: Request, res: Response, next: NextFunction): void => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+          res.status(400).json({errors: errors.array()});
+    }
+    next();
+ 
+};
+
 export default {
-    retrieveUserReviews
+    retrieveUserReviews,
+    validate
 }
