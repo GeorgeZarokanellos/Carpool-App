87cdc8d1bc5f273df338e226f2037bf6acd77a1b
GeorgeZarokanellos <geo.zarak@gmail.com>
2024-04-01 12:36:31 +0300

    added test file

--- database/test.sql
+++ database/test.sql
@@ -2,7 +2,16 @@ select * from app_user;
 select *
 from trip;
 
-ALTER SEQUENCE trip_trip_id_seq RESTART WITH 1;
 
-delete
-from trip;
+ALTER SEQUENCE trip_trip_id_seq RESTART WITH 1;
+ALTER SEQUENCE chat_chat_id_seq RESTART WITH 1;
+
+ALTER TABLE trip ADD COLUMN trip_creator_id INT NOT NULL ;
+
+--delete all tables
+
+delete from trip;
+delete from chat;
+delete from chat_participants;
+delete from trip_passengers;
+delete from trip_stops;
