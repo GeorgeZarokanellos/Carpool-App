67d2ac34e19127c01e74f348586dd01862c64f47
GeorgeZarokanellos <geo.zarak@gmail.com>
2024-05-10 22:08:05 +0300

    Centered the trip information components

--- frontend/carpool-app/src/pages/SearchTrips.tsx
+++ frontend/carpool-app/src/pages/SearchTrips.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonGrid, IonHeader, IonItem, IonPage, IonTitle, IonToolbar, IonRow, IonCol } from '@ionic/react';
 import { TripInformation } from '../components/TripInformation';
 import './SearchTrips.css';
 
@@ -12,8 +11,15 @@ const Tab3: React.FC = () => {
           <IonTitle>Search Available Trips</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <TripInformation time='15:00' origin='aretha' noOfPassengers={2} noOfStops={2} />
+      <IonContent fullscreen >
+        <IonGrid >
+          <IonRow className='ion-justify-content-center'>
+            <IonCol size='3'>
+              <TripInformation time='15:00' origin='aretha' noOfPassengers={2} noOfStops={2} finish='University'/>
+              <TripInformation time='15:00' origin='aretha' noOfPassengers={2} noOfStops={2} finish='University'/>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
