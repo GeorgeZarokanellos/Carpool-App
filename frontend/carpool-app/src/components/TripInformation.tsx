718f09edf123cc53581eeb15a738bea7fb962e0f
GeorgeZarokanellos <geo.zarak@gmail.com>
2024-05-10 22:07:34 +0300

    Added the third column for driver image, name and rating

--- frontend/carpool-app/src/components/TripInformation.tsx
+++ frontend/carpool-app/src/components/TripInformation.tsx
@@ -1,40 +1,64 @@
 import React from "react";
 import { IonCard, IonCardContent, IonCardHeader, IonIcon, IonItem, IonGrid, IonRow, IonCol, IonLabel } from "@ionic/react";
 import { TripProps } from "../interfacesAndTypes/Interfaces";
-import { timeOutline, peopleOutline, pinOutline, locationOutline, flagOutline } from 'ionicons/icons';
+import { 
+        timeOutline, 
+        peopleOutline, 
+        pinOutline, 
+        locationOutline, 
+        flagOutline,
+        star,
+        car,
+    } from 'ionicons/icons';
 import './TripInformation.css';
 
-export const TripInformation: React.FC<TripProps>  = ({time, origin, noOfPassengers, noOfStops}) => (
-    <IonCard>
+export const TripInformation: React.FC<TripProps>  = ({time, origin, noOfPassengers, noOfStops, finish}) => (
+    <IonCard className="trip-info-container">
         <IonCardHeader></IonCardHeader>
-        <IonCardContent>
+        <IonCardContent className="card-content">
             <IonGrid>
                 <IonRow>
-                    <IonCol>
+                    <IonCol size="4">
                         <div className="">
-                            <IonItem>
+                            <IonItem lines="none">
                                 <IonIcon icon={timeOutline} slot="start" className="time-icon"/>
                                 <IonLabel>{time}</IonLabel>
                             </IonItem>
-                            <IonItem>
+                            <IonItem lines="none">
                                 <IonIcon icon={peopleOutline} slot="start" className="people-icon"/>
                                 {noOfPassengers}
                             </IonItem>
-                            <IonItem>
+                            <IonItem lines="none">
                                 <IonIcon icon={pinOutline} slot="start" className="pin-icon"/>
                                 {noOfStops}
                             </IonItem>
                         </div>
                     </IonCol>
-                    <IonCol>
+                    <IonCol size="4">
                         <div className="">
-                            <IonItem>
+                            <IonItem lines="none">
                                 <IonIcon icon={locationOutline} slot="start" className="location-icon"/>
                                 {origin}
                             </IonItem>
-                            <IonItem>
-                                <IonIcon icon={flagOutline} className="flag-icon"/>
+                            <IonItem lines="none">
+                                <IonIcon icon={flagOutline} slot="start" className="flag-icon"/>
+                                {finish}
                             </IonItem>
+                        </div>
+                    </IonCol>
+                    <IonCol size="4">
+                        <div>
+                            <IonItem lines="none">
+                                George
+                                <IonIcon icon={car} className="flag-icon"/>
+                            </IonItem>
+                            <div className="stars">
+                                <IonIcon icon={star} className="star-icon" />
+                                <IonIcon icon={star} className="star-icon" />
+                                <IonIcon icon={star} className="star-icon" />
+                                <IonIcon icon={star} className="star-icon" />
+                                <IonIcon icon={star} className="star-icon" />
+                            </div>
                         </div>
                     </IonCol>
                 </IonRow>
