efaedbf00ac27df5243ea09a8f61d19f962cd88c
GeorgeZarokanellos <geo.zarak@gmail.com>
2024-05-12 00:15:57 +0300

    Minor changes

--- frontend/carpool-app/src/App.tsx
+++ frontend/carpool-app/src/App.tsx
@@ -14,7 +14,7 @@ import { IonReactRouter } from '@ionic/react-router';
 import { map, chatbox, notifications, person, search} from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
+import Tab3 from './pages/SearchTrips';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
