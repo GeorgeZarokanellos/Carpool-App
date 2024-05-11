a75e6ba7a69fd0cad78d0b9667418b4c1b918d1e
GeorgeZarokanellos <geo.zarak@gmail.com>
2024-05-12 00:08:00 +0300

    Search trip now iterates over the trips array to display each trip contained inside

--- frontend/carpool-app/src/pages/SearchTrips.tsx
+++ frontend/carpool-app/src/pages/SearchTrips.tsx
@@ -1,23 +1,54 @@
-import React from 'react';
-import { IonContent, IonGrid, IonHeader, IonItem, IonPage, IonTitle, IonToolbar, IonRow, IonCol } from '@ionic/react';
+import React, { useEffect } from 'react';
+import { IonContent, IonGrid, IonHeader, IonItem, IonPage, IonTitle, IonToolbar, IonRow, IonCol, IonSearchbar } from '@ionic/react';
 import { TripInformation } from '../components/TripInformation';
+import { Trip } from '../interfacesAndTypes/Types';
 import './SearchTrips.css';
+import instance from '../AxiosConfig';
+
+
 
 const Tab3: React.FC = () => {
+  const [trips, setTrips] = React.useState<Trip[]>([]);
+  let formattedDate;
+  let formattedTime;
+
+  useEffect(() => {
+    instance.get('/trips')
+    .then(response => {
+      console.log(response.data);
+      setTrips(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  
+  },[])
+
+
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Search Available Trips</IonTitle>
-        </IonToolbar>
+      <IonHeader className='ion-no-border'>
+        <IonSearchbar placeholder='Search available trips' animated={true} class='custom'/>
       </IonHeader>
       <IonContent fullscreen >
         <IonGrid >
           <IonRow className='ion-justify-content-center'>
-            <IonCol size='3'>
-              <TripInformation time='15:00' origin='aretha' noOfPassengers={2} noOfStops={2} finish='University'/>
-              <TripInformation time='15:00' origin='aretha' noOfPassengers={2} noOfStops={2} finish='University'/>
-            </IonCol>
+            {trips.map((trip,index) => {
+              const date = new Date(trip.startingTime);
+              formattedDate = date.toLocaleDateString();
+              formattedTime = date.toLocaleTimeString();
+              return(
+                <TripInformation 
+                  key={trip.tripId}
+                  startingTime={formattedTime} 
+                  dateOfTrip={formattedDate} 
+                  origin={trip.startLocation}
+                  noOfPassengers={trip.noOfPassengers}
+                  noOfStops={trip.noOfStops}
+                  finish='Pritaneia'
+                  />
+              )
+            })}
           </IonRow>
         </IonGrid>
       </IonContent>
