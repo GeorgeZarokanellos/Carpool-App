17d5f866cdd6d75f3863be1a8ec02e00e7f429dd
GeorgeZarokanellos <geo.zarak@gmail.com>
2024-05-23 16:34:45 +0300

    Moved the main page to a seperate component and added a new login page

--- frontend/carpool-app/src/App.tsx
+++ frontend/carpool-app/src/App.tsx
@@ -2,19 +2,11 @@ import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import {
   IonApp,
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { map, chatbox, notifications, person, search} from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/SearchTrips';
+import Login from './pages/Login';
+import { Main } from './pages/Main';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -40,40 +32,13 @@ setupIonicReact();
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
-          </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
-          </Route>
-          <Route path="/tab3">
-            <Tab3 />
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/tab1" />
-          </Route>
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon aria-hidden="true" icon={map} />
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon aria-hidden="true" icon={chatbox} />
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon aria-hidden="true" icon={search} />
-          </IonTabButton>
-          <IonTabButton tab="tab4" href="/tab4">
-            <IonIcon aria-hidden="true" icon={notifications} />
-          </IonTabButton>
-          <IonTabButton tab="tab5" href="/tab5">
-            <IonIcon aria-hidden="true" icon={person} />
-            {/* <IonLabel>Tab 5</IonLabel> */}
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
+      <Route path ="/login">
+        <Login />
+      </Route>
+      <Route path="/main">
+        <Main />
+      </Route>
+      <Redirect exact from="/" to="/login" />
     </IonReactRouter>
   </IonApp>
 );
