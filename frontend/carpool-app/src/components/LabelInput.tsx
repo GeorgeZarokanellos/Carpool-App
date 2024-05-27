984234d43de3e8bfec49e1a48f77ec3d4ca690af
GeorgeZarokanellos <geo.zarak@gmail.com>
2024-05-27 16:02:09 +0300

    Made a component that creates a label and an input based on the arguments props to use in registration where many fields are required

---
+++ frontend/carpool-app/src/components/LabelInput.tsx
@@ -0,0 +1,22 @@
+// LabelInput.tsx
+import React from 'react';
+import { IonLabel, IonInput } from '@ionic/react';
+import './LabelInput.scss';
+import { LabelInputProps } from '../interfacesAndTypes/Interfaces';
+
+export const LabelInput: React.FC<LabelInputProps<string | number>> = ({ label, value, type, onIonChange }) => {
+  return (
+    <div className='label-input'>
+      <IonLabel>{label}</IonLabel>
+      <IonInput 
+        value={value}
+        type={type}
+        onIonChange={e => {
+          if (e.detail.value != undefined) {
+            onIonChange(e.detail.value as string | number);
+          }
+        }}
+      />
+    </div>
+  );
+};
