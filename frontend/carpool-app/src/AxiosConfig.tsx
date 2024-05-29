c0b17456844007d5281c71208263ef60ede06475
GeorgeZarokanellos <geo.zarak@gmail.com>
2024-05-29 15:14:55 +0300

    Added withCredentials attribute so that requests include the credentials which the user was logged in and interceptors that check whether the response in any request has 401 status at which point the user is redirected to login page

--- frontend/carpool-app/src/AxiosConfig.tsx
+++ frontend/carpool-app/src/AxiosConfig.tsx
@@ -1,11 +1,24 @@
 import axios from 'axios';
+import { useHistory } from 'react-router';
 
 const instance = axios.create({
   baseURL: 'http://localhost:3000/api/v1',
   headers: {
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer your_token_here' 
-  }
+    'Authorization': 'Bearer your_token_here',
+  },
+  withCredentials: true,
 });
+
+instance.interceptors.response.use(
+  response => response,
+  error => {
+    if(error.response && error.response.status === 401){
+      window.location.href = '/';
+    } 
+    return Promise.reject(error);
+  }
+)
+
 
 export default instance;
