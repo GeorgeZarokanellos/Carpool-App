eb3982e9016426f87d9e54e209ea662d550aedfb
GeorgeZarokanellos <geo.zarak@gmail.com>
2024-05-29 15:21:30 +0300

    Added refreshkey state that is been passed to the searchTrip component in order to refresh the get trips request every time the user navigates to the search trip tab

--- frontend/carpool-app/src/pages/Main.tsx
+++ frontend/carpool-app/src/pages/Main.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import { IonRouterOutlet, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/react';
+import React, { useState } from 'react';
+import { Route} from 'react-router-dom';
+import { IonRouterOutlet, IonTabs, IonTabBar, IonTabButton, IonIcon} from '@ionic/react';
 import { map, chatbox, notifications, person, search } from 'ionicons/icons';
 import Tab1 from './Tab1';
 import Tab2 from './Tab2';
@@ -8,15 +8,22 @@ import SearchTrips from './SearchTrips';
 import Profile from './Profile';
 
 export const Main: React.FC = () => {
+  //for testing purposes 
+  const [refreshKey, setRefreshKey] = useState(0);
+  const refreshTrips = () => {
+    setRefreshKey(refreshKey => refreshKey + 1)
+  }
+  // const { path } = useRouteMatch(); // path is the path of the current route
 
  return (
     <IonTabs>
         <IonRouterOutlet>
           <Route path="/main/tab1" component={Tab1} />
           <Route path="/main/tab2" component={Tab2} />
-          <Route path="/main/search-trips" component={SearchTrips}/>
+          <Route path="/main/search-trips" render={() => <SearchTrips refreshKey={refreshKey}/>}/>
           <Route path="/main/tab4" />
-          <Route exact path="/main/profile" component={Profile} />
+          <Route path="/main/profile" component={Profile} />
+
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/main/tab1">
@@ -25,7 +32,7 @@ export const Main: React.FC = () => {
           <IonTabButton tab="tab2" href="/main/tab2">
             <IonIcon aria-hidden="true" icon={chatbox} />
           </IonTabButton>
-          <IonTabButton tab="searchTrips" href="/main/search-trips">
+          <IonTabButton tab="searchTrips" href="/main/search-trips" onClick={refreshTrips}>
             <IonIcon aria-hidden="true" icon={search} />
             {/* <IonLabel>Search Trips</IonLabel> */}
           </IonTabButton>
