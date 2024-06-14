4321bf41d8e01bff32ad5a8b7c94122952f59e37
George Zarokanellos <geo.zarak@gmail.com>
2024-06-14 16:45:44 +0300

    Changed the dimensions of the page to the viewable screen for better visibility on browsers. Used swiper instead of react carousel for better mobile experience.

--- frontend/carpool-app/src/pages/DriverVehicleRegistration.tsx
+++ frontend/carpool-app/src/pages/DriverVehicleRegistration.tsx
@@ -1,18 +1,22 @@
+//TODO fix the problem with the carousel display
+
 import {
   IonButton,
   IonContent,
+  IonFooter,
+  IonGrid,
   IonHeader,
-  IonInput,
   IonPage,
   IonPicker,
+  IonRow,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
 import React, { useCallback, useState } from "react";
 import type { autoMaker } from "../interfacesAndTypes/Types";
 import "./DriverVehicleRegistration.scss";
-import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Swiper,SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 export const DriverVehicleRegistration: React.FC = () => {
   const [showMakerPicker, setShowMakerPicker] = useState(false);
@@ -24,14 +28,17 @@ export const DriverVehicleRegistration: React.FC = () => {
   });
   const [selectedVehicleModel, setSelectedVehicleModel] = useState<string>("");
   const [noOfSeats, setNoOfSeats] = useState<number>();
-  const [driversLicense, setDriversLicense] = useState<Blob | null>(null);
+  const [driversLicense, setDriversLicense] = useState<Blob>();
   const [driversLicenseFileName, setDriversLicenseFileName] = useState<string>(""); 
-  const [vehicleInsurance, setVehicleInsurance] = useState<Blob | null>(null);
+  const [vehicleInsurance, setVehicleInsurance] = useState<Blob>();
   const [vehicleInsuranceFileName, setVehicleInsuranceFileName] = useState<string>("");
-  const [vehicleRegistration, setVehicleRegistration] = useState<Blob | null>(null);
+  const [vehicleRegistration, setVehicleRegistration] = useState<Blob>();
   const [vehicleRegistrationFileName, setVehicleRegistrationFileName] = useState<string>("");
   const [vehiclePictures, setVehiclePictures] = useState<Blob[]>([]);
   const [imageUrls, setImageUrls] = useState<string[]>([]);
+
+  const viewportWidth = window.innerWidth;
+  const viewportHeight = window.innerHeight;
 
   const autoMakers = [
     { maker: "Toyota", models: ["Yaris", "Avensis"] },
@@ -47,11 +54,16 @@ export const DriverVehicleRegistration: React.FC = () => {
     e.preventDefault();
   };
 
-  const createFileUploadButton = (id: string, fileName: string, setMethod: React.Dispatch<React.SetStateAction<string>>) => {
+  const createFileUploadButton = (id: string, 
+                                  fileName: string, 
+                                  setFileMethod: React.Dispatch<React.SetStateAction<Blob | undefined>>, 
+                                  setNameMethod: React.Dispatch<React.SetStateAction<string>>) => {
     const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
-      if (e.target.files && e.target.files[0] && e.target.files[0].name !== undefined) {
-        console.log(e.target.files[0].name);
-        setMethod(e.target.files[0].name);
+      if (e.target.files?.[0]?.name !== undefined) {
+        console.log(e.target.files[0]);
+        // console.log(e.target.files[0].name);
+        setFileMethod(e.target.files[0]);
+        setNameMethod(e.target.files[0].name);
       }
     }
     const displayedName = fileName ? fileName : id.split(/(?=[A-Z])/).join(' ');
@@ -72,174 +84,191 @@ export const DriverVehicleRegistration: React.FC = () => {
     }
   }, []);
 
+  
+
   return (
-    <IonPage>
+    <IonPage style={{width: `${viewportWidth}`, height: `${viewportHeight}`}}>
       <IonHeader>
         <IonToolbar>
-          <IonTitle style={{ textAlign: "center" }}> Driver & Vehicle Registration </IonTitle>
+          <IonTitle style={{ textAlign: "center" }}>
+            <div style={{whiteSpace: "normal"}}>
+              Driver & Vehicle Registration 
+            </div>
+          </IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <div className="driver-vehicle-registration-container">
-          <form
-            onSubmit={handleDriverVehicleRegistration}
-            className="custom-form"
-          >
-            <div className="pickers">
-                <IonButton onClick={() => setShowMakerPicker(true)}>
-                {
-                  selectedVehicleMaker.maker && selectedVehicleModel 
-                  ? (
-                      //TODO different orientation to look better?
-                      <>  
-                        <p style={{margin: 0}}>Selected vehicle
-                          <br />
-                          {selectedVehicleMaker.maker} {selectedVehicleModel}
-                        </p>
-                      </>
-                    ) 
-                    : 'Select Maker and Model'
-                }
-                </IonButton>
-                {showMakerPicker && (
-                  <IonPicker
-                    isOpen={showMakerPicker}
-                    columns={[
-                      {
-                        name: "Car Maker",
-                        options: autoMakers.map((autoMaker, index) => ({
-                          text: autoMaker.maker,
-                          value: index,
-                        })),
-                      },
-                    ]}
-                    buttons={[
-                      {
-                        text: "Cancel",
-                        role: "Cancel",
-                        handler: () => setShowMakerPicker(false),
-                      },
-                      {
-                        text: "Confirm",
-                        handler: (value) => {
-                          console.log(
-                            "selected automaker before set:",
-                            value["Car Maker"]
-                          );
-
-                          setSelectedVehicleMaker(
-                            autoMakers[value["Car Maker"].value]
-                          );
-                          setShowMakerPicker(false);
-                          setShowModelPicker(true);
-                        },
-                      },
-                    ]}
-                  />
-                )}
-                {showModelPicker && (
-                  <IonPicker
-                    isOpen={showModelPicker}
-                    columns={[
-                      {
-                        name: "Model",
-                        options: selectedVehicleMaker.models.map(
-                          (model, index) => ({
-                            text: model,
-                            value: index,
-                          })
-                        ),
-                      },
-                    ]}
-                    buttons={[
-                      {
-                        text: "Back to Maker",
-                        role: "Back",
-                        handler: () => {
-                          setShowMakerPicker(true);
-                          setShowModelPicker(false);
-                        },
-                      },
-                      {
-                        text: "Confirm",
-                        handler: (value) => {
-                          console.log("selected model:", value["Model"].text);
-                          setSelectedVehicleModel(value["Model"].text);
-                          setShowModelPicker(false);
-                        },
-                      },
-                    ]}
-                  />
-                )}
-            </div>
-            <div className="no-of-seats-container"> 
-              <IonButton onClick={() => setShowNoOfSeatsPicker(true)}>
-                {noOfSeats ? 'Available seats: ' + noOfSeats : "Select number of seats"}
-              </IonButton>
-              <IonPicker 
-                isOpen={showNoOfSeatsPicker}
-                columns={[
-                  {
-                    name: 'Seats',
-                    options: [
-                      { text: '1', value: 1 },
-                      { text: '2', value: 2 },
-                      { text: '3', value: 3 },
-                      { text: '4', value: 4 },
-                      { text: '5', value: 5 }
-                    ]
-                  }
-                ]}
-                buttons={[
-                  {
-                    text: 'Cancel',
-                    role: 'Cancel',
-                    handler: () => setShowNoOfSeatsPicker(false)
-                  },
-                  {
-                    text: 'Confirm',
-                    handler: (value) => {
-                      // console.log(value.Seats.value);
-                      setNoOfSeats(value.Seats.value);
-                      setShowNoOfSeatsPicker(false);
+        <IonGrid>
+          <IonRow className="filler"/>
+          <IonRow>
+            <form
+              onSubmit={handleDriverVehicleRegistration}
+              className="custom-form"
+            >
+              <div className="form-contents">
+                <div className="pickers">
+                    <IonButton onClick={() => setShowMakerPicker(true)}>
+                    {
+                      selectedVehicleMaker.maker && selectedVehicleModel 
+                      ? (
+                          //TODO different orientation to look better?
+                          <>  
+                            <p style={{margin: 0}}>Selected vehicle
+                              <br />
+                              {selectedVehicleMaker.maker} {selectedVehicleModel}
+                            </p>
+                          </>
+                        ) 
+                        : 'Select Maker and Model'
                     }
-                  }
-                ]}
-              />
-            </div>
-            <div className="drivers-license-container">
-              {createFileUploadButton( "driversLicense", driversLicenseFileName, setDriversLicenseFileName)}
-            </div>
-            <div className="vehicle-insurance-container">
-              {createFileUploadButton( "vehicleInsurance", vehicleInsuranceFileName, setVehicleInsuranceFileName)}
-            </div>
-            <div className="vehicle-registration-container">
-              {createFileUploadButton( "vehicleRegistration", vehicleRegistrationFileName, setVehicleRegistrationFileName)}
-            </div>
-            <div className="car-images-container">
-              <IonButton onClick={() => document.getElementById('vehiclePictures')?.click()}>
-                {vehiclePictures.length ? 'Selected images' : 'Upload car images'}
-                <input type="file" id="vehiclePictures" hidden multiple accept="image/*" onChange={handleImagesUpload} />
-              </IonButton>
-              {/* {console.log(imageUrls)} */}
-              <Carousel>
-                {
-                  imageUrls.map((url, index) => (
-                    <div key={index}>
-                      <img src={url} alt="car" />
-                    </div>
-                  ))
-                }
-              </Carousel>
-            </div>
-            <div className="register-button-container">
-              <IonButton expand="full" type="submit" shape="round" className="register-button">
-                Register
-              </IonButton>
-            </div>
-          </form>
-        </div>
+                    </IonButton>
+                    {showMakerPicker && (
+                      <IonPicker
+                        isOpen={showMakerPicker}
+                        columns={[
+                          {
+                            name: "Car Maker",
+                            options: autoMakers.map((autoMaker, index) => ({
+                              text: autoMaker.maker,
+                              value: index,
+                            })),
+                          },
+                        ]}
+                        buttons={[
+                          {
+                            text: "Cancel",
+                            role: "Cancel",
+                            handler: () => setShowMakerPicker(false),
+                          },
+                          {
+                            text: "Confirm",
+                            handler: (value) => {
+                              console.log(
+                                "selected automaker before set:",
+                                value["Car Maker"]
+                              );
+
+                              setSelectedVehicleMaker(
+                                autoMakers[value["Car Maker"].value]
+                              );
+                              setShowMakerPicker(false);
+                              setShowModelPicker(true);
+                            },
+                          },
+                        ]}
+                      />
+                    )}
+                    {showModelPicker && (
+                      <IonPicker
+                        isOpen={showModelPicker}
+                        columns={[
+                          {
+                            name: "Model",
+                            options: selectedVehicleMaker.models.map(
+                              (model, index) => ({
+                                text: model,
+                                value: index,
+                              })
+                            ),
+                          },
+                        ]}
+                        buttons={[
+                          {
+                            text: "Back to Maker",
+                            role: "Back",
+                            handler: () => {
+                              setShowMakerPicker(true);
+                              setShowModelPicker(false);
+                            },
+                          },
+                          {
+                            text: "Confirm",
+                            handler: (value) => {
+                              console.log("selected model:", value["Model"].text);
+                              setSelectedVehicleModel(value["Model"].text);
+                              setShowModelPicker(false);
+                            },
+                          },
+                        ]}
+                      />
+                    )}
+                </div>
+                <div className="no-of-seats-container"> 
+                  <IonButton onClick={() => setShowNoOfSeatsPicker(true)}>
+                    {noOfSeats ? 'Available seats: ' + noOfSeats : "Select number of seats"}
+                  </IonButton>
+                  <IonPicker 
+                    isOpen={showNoOfSeatsPicker}
+                    columns={[
+                      {
+                        name: 'Seats',
+                        options: [
+                          { text: '1', value: 1 },
+                          { text: '2', value: 2 },
+                          { text: '3', value: 3 },
+                          { text: '4', value: 4 },
+                          { text: '5', value: 5 }
+                        ]
+                      }
+                    ]}
+                    buttons={[
+                      {
+                        text: 'Cancel',
+                        role: 'Cancel',
+                        handler: () => setShowNoOfSeatsPicker(false)
+                      },
+                      {
+                        text: 'Confirm',
+                        handler: (value) => {
+                          // console.log(value.Seats.value);
+                          setNoOfSeats(value.Seats.value);
+                          setShowNoOfSeatsPicker(false);
+                        }
+                      }
+                    ]}
+                  />
+                </div>
+                <div className="drivers-license-container">
+                  {createFileUploadButton( "driversLicense", driversLicenseFileName, setDriversLicense , setDriversLicenseFileName)}
+                </div>
+                <div className="vehicle-insurance-container">
+                  {createFileUploadButton( "vehicleInsurance", vehicleInsuranceFileName, setVehicleInsurance, setVehicleInsuranceFileName)}
+                </div>
+                <div className="vehicle-registration-container">
+                  {createFileUploadButton( "vehicleRegistration", vehicleRegistrationFileName, setVehicleRegistration, setVehicleRegistrationFileName)}
+                </div>
+                <div className="car-images-container">
+                  <IonButton onClick={() => document.getElementById('vehiclePictures')?.click()}>
+                    {vehiclePictures.length ? 'Selected images' : 'Upload car images'}
+                    <input type="file" id="vehiclePictures" hidden multiple accept="image/*" onChange={handleImagesUpload} />
+                  </IonButton>
+                  <Swiper
+                    spaceBetween={50}
+                    slidesPerView={1}
+                    onSlideChange={() => console.log('slide change')}
+                  >
+                    {imageUrls.map((url, index) => (
+                      <SwiperSlide key={index}>
+                        <img src={url} alt="car" />
+                      </SwiperSlide>
+                    ))}
+                  </Swiper>
+                </div>
+              </div>
+            </form>
+          </IonRow>
+          <IonRow className="filler"/>
+        </IonGrid>
       </IonContent>
+      <IonFooter>
+        <IonToolbar>
+          <div className="register-button-container">
+            <IonButton className="register-button" expand="block" onClick={handleDriverVehicleRegistration}>
+              Submit
+            </IonButton>
+          </div>
+        </IonToolbar>
+      </IonFooter>
     </IonPage>
   );
 };
