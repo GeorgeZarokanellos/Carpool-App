035a89cc368a45912565ca76b2980e9e97145723
George Zarokanellos <geo.zarak@gmail.com>
2024-07-01 18:43:28 +0300

    Upload of files is now a separate asynchronous function, dir path is full instead of relative, promises are now all handled correctly

--- backend/src/controller/registration_controller.ts
+++ backend/src/controller/registration_controller.ts
@@ -9,6 +9,7 @@ import Driver from '../model/driver';
 import Vehicle from '../model/vehicle';
 import sequelize from '../database/connect_to_db';
 import { type addUserRequestBodyInterface, type carRegisterRequestBodyInterface} from '../interface/trip_interface';
+import path from 'path';
 
 const saltRounds = 10;
 
@@ -18,25 +19,114 @@ interface MulterFile {
 }
 // #endregion
 
-export const findUsernameAndInitializeUpload = (req: Request, res: Response, next: NextFunction): void => {
-    async function findUsernameAndInitializeUploadAsync(): Promise<void> {
-        try {
-            const userId = req.params.id;
-            const user: User | null = await User.findByPk(userId);
-            if(user !== null)
-                initializeUpload(user.username)(req, res ,next);
-            else
-                res.status(404).send('User not found');
-        } catch(err) {
-            if(err instanceof Error)
-                console.error('Error from findUsernameAndInitializeUploadAsync:' + err.message);
-            else if (typeof err === 'string')
-                console.error('Error from findUsernameAndInitializeUploadAsync:' + err);
-            res.status(500).send(err);
-        }
+const uploadFiles = async (
+        req: Request,
+        res: Response,
+        storage: multer.StorageEngine, 
+        fileFilter: (req: Request, file: MulterFile, cb: FileFilterCallback) => void
+    ): Promise<void> => {
+    try {
+        await new Promise((resolve,reject) => {
+            multer({storage, fileFilter}).fields([ // specify the fields to be uploaded
+               //multer uses the name attribute of the input field to determine the field name
+               {name: 'driversLicense', maxCount: 1},  
+               {name: 'vehicleRegistration', maxCount: 1},
+               {name: 'vehicleInsurance', maxCount: 1},
+               {name: 'vehicleImages', maxCount: 4},
+           ])(req,res, (err)=>{
+            if(err)
+                reject(err)
+            else {
+                
+                if (req.files && Object.keys(req.files).length === 0) {
+                    console.warn("No files were uploaded.");
+                } else {
+                    console.log("Uploaded files:", req.files);
+                }
+                resolve(req);
+            }
+           });
+        })
+    } catch (error) {
+        console.log("error from uploadFilesSync", error);
     }
-    findUsernameAndInitializeUploadAsync().catch(next);
 }
+
+const initializeUpload = async (req:Request, res:Response, next: NextFunction , username: string): Promise<void> => {
+    // store the uploaded files in the uploads folder
+    const Storage = multer.diskStorage({   
+        destination: function(req,file,cb){    // cb is callback function that takes an error and a destination folder as parameters
+            if(!username){
+                throw new Error('Username is missing');
+            }
+            const dir = path.resolve(`/home/george/Desktop/Carpool App/backend/static/uploads/${username}`);   // create a folder for each driver using the username
+            fs.access(dir, fs.constants.F_OK, (err) => {   // check if the folder already exists
+                if(err !== null){    // err here means that the folder doesn't exist
+                    fs.mkdir(dir, {recursive:true}, (err)=>{  // create the folder
+                        if(err !== null){   // error when creating the folder 
+                            console.log('Error from mkdir in initializeUpload:' + err.message);
+                            cb(err, '');
+                        }
+                        else 
+                            cb(null, dir);// if the folder doesn't exist, create it
+                    });
+                } else {
+                    cb(null, dir);  // if the folder already exists, use it
+                }
+            })
+        },
+        filename: function(req,file,cb){
+            cb(null, Date.now() + '-' + file.originalname); // give the file a name that includes the current date and time
+        }
+    });
+
+    const fileFilter = (req: Request, file: MulterFile, cb: FileFilterCallback): void => {  // checks if the file is an image or pdf
+        console.log("file type",file.mimetype);
+        
+        if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'application/pdf'){
+            console.log("Accepted the file", file);
+            
+            cb(null, true); // accept the file
+        }
+        else
+            cb(null, false);    // reject the file
+    };
+
+    try {
+        await uploadFiles(req, res, Storage, fileFilter);
+        next();
+    } catch (error) {
+        console.log("Error from initialize upload in upload files");
+        
+    }
+}
+
+export const findUsernameAndInitializeUpload = async (req:Request, res: Response, next: NextFunction , driverId: number): Promise<void> => {
+    try {
+        const user: User | null = await User.findByPk(driverId);
+        // console.log("User from findUsername", user);
+        
+        if (user !== null) {
+            const username: string = user.username;
+            try {
+                await initializeUpload(req,res, next,username); // initialize the upload function
+            } catch (error) {
+                console.log("error in initializing upload", error);
+                // next(error);
+            }
+        } else {
+            res.status(404).send('User not found from initialization of upload');
+            // return Promise.reject('User not found from initialization of upload');
+        }
+    } catch (err) {
+        if (err instanceof Error)
+            console.error('Error from findUsernameAndInitializeUpload:' + err.message);
+        else if (typeof err === 'string')
+            console.error('Error from findUsernameAndInitializeUpload:' + err);
+        res.status(500).send(err);
+        // return Promise.reject(err);
+    }
+};
 
 export const addUser = (req: Request, res: Response, next: NextFunction): void => {
     async function addUserAsync(): Promise<void> {
@@ -86,7 +176,7 @@ export const addUser = (req: Request, res: Response, next: NextFunction): void =
                     role,
                     phone
                 }, {transaction});
-                res.status(200).send(newUser);
+                res.status(200).json(newUser);
 
             });
         } catch (err) {
@@ -103,54 +193,33 @@ export const addUser = (req: Request, res: Response, next: NextFunction): void =
 export const addDriverAndVehicle = (req: Request, res: Response, next: NextFunction): void => {
     async function addDriverAndVehicleAsync(): Promise<void> {
         try {
-            const driverId = req.params.id;
-            const username: string = req.body.username;
-            //TODO vehicle now has owner id field change logic accordingly
-            const {vehicleId, ownerId, carMaker, carModel, carCapacity}: carRegisterRequestBodyInterface = req.body;
-            const requiredFields = ['vehicleId', 'ownerId' , 'carMaker', 'carModel', 'carCapacity']
-            for (const field of requiredFields){
-                if(req.body[field] !== undefined || req.body[field] !== null || req.body[field] !== ''){
-                    res.status(400).send(`${field} is missing`);
-                    return;
-                }
-            }
-            const upload = initializeUpload(username); // initialize the upload function
-            try{
-                await new Promise<void> ((resolve,reject) => {
-                    upload(req, res, (err) => { // upload the files
-                        if(err !== null){
-                            reject(err);
-                            // console.log('Error from upload in addDriver:' + err);
-                        }
-                        else    
-                            resolve();
-                    });
-                })
-            } catch(err) { 
-                if(err instanceof Error)
-                    console.error('Error from upload in addDriver:' + err.message);
-                else if (typeof err === 'string')
-                    console.error('Error from upload in addDriver:' + err);
-                res.status(500).send(err);
-            }
+            const driverId = Number(req.params.id);
+            // const bodyFromMulter: carRegisterRequestBodyInterface =  await findUsernameAndInitializeUpload(req, res, next, driverId);
+            const {plateNumber, maker, model, noOfSeats} = req.body;
+            // console.log('body from multer', bodyFromMulter);
 
+            // create a new driver and vehicle in a transaction
+            
             await sequelize.transaction(async (transaction) => {
                 const newDriver = await Driver.create({ // create a new driver
                     // fields
-                    driverId
+                    driverId,
+                    licenseId: 90341, // temporary value
                 }, {transaction});
 
                 const newVehicle = await Vehicle.create({   // create a new vehicle
                     // fields
-                    plateNumber: vehicleId,
-                    ownerId: ownerId,
-                    maker: carMaker,
-                    model: carModel,
-                    noOfSeats: carCapacity
+                    plateNumber,
+                    ownerId: driverId,  
+                    maker,
+                    model,
+                    noOfSeats
                 }, {transaction});
 
-                res.status(200).send({driver: newDriver, vehicle: newVehicle});
+                res.status(200).send({driver: newDriver, vehicle: newVehicle, files: req.files});
             });
+
+
         } catch(err) {
             if(err instanceof Error)
                 console.error('Error from addDriverAndVehicleAsync:' + err.message);
@@ -162,41 +231,3 @@ export const addDriverAndVehicle = (req: Request, res: Response, next: NextFunct
     addDriverAndVehicleAsync().catch(next);
 }   
 
-const initializeUpload = (username: string): (req: Request, res: Response, next: NextFunction) => void => {
-    const storage = multer.diskStorage({    // store the uploaded files in the uploads folder
-        destination: function(req,file,cb){    // cb is callback function that takes an error and a destination folder as parameters
-            const dir = `./uploads/drivers/${username}`;   // create a folder for each driver using the username
-            fs.access(dir, fs.constants.F_OK, (err) => {   // check if the folder already exists
-                if(err !== null){    // err here means that the folder doesn't exist
-                    fs.mkdir(dir, (err)=>{  // create the folder
-                        if(err !== null){   // error when creating the folder 
-                            console.log('Error from mkdir in initializeUpload:' + err.message);
-                            cb(err, '');
-                        }
-                        else 
-                            cb(null, dir);// if the folder doesn't exist, create it
-                    });
-                } else {
-                    cb(null, dir);  // if the folder already exists, use it
-                }
-            })
-        },
-        filename: function(req,file,cb){
-            cb(null, Date.now() + '-' + file.originalname); // give the file a name that includes the current date and time
-        }
-    });
-
-    const fileFilter = (req: Request, file: MulterFile, cb: FileFilterCallback): void => {  // checks if the file is an image or pdf
-        if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'application/pdf')
-            cb(null, true); // accept the file
-        else
-            cb(null, false);    // reject the file
-    };
-
-    return multer({ storage, fileFilter }).fields([ // specify the fields to be uploaded
-        {name: 'driversLicense', maxCount: 1},  //TODO take the id of the license
-        {name: 'carsRegistration', maxCount: 1},
-        {name: 'carsInsurance', maxCount: 1},
-        {name: 'carImages', maxCount: 4},
-    ]);
-}
