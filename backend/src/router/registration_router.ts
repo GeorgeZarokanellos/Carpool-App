a3b97974b29e7472a365e98918c633b9f6fbb66e
George Zarokanellos <geo.zarak@gmail.com>
2024-07-03 13:56:06 +0300

    Now correctly awaits the findUsernameAndInitializeUpload function to be excuted before calling addDriverAndVehicle

--- backend/src/router/registration_router.ts
+++ backend/src/router/registration_router.ts
@@ -2,10 +2,27 @@ import express from 'express';
 import { addUser, findUsernameAndInitializeUpload, addDriverAndVehicle } from '../controller/registration_controller';
 import {newUserValidationRules, newDriverAndVehicleValidationRules} from '../util/validator/registration_validator';
 import {validate} from '../util/common_functions';
+
+//TODO: add validation for driver-vehicle post request  
+
 const router = express.Router();
 
+router.post('/user', 
+            newUserValidationRules(), 
+            validate, 
+            addUser
+        );
 
-router.post('/user', newUserValidationRules(), validate, addUser);
-router.post('/driver/:id', newDriverAndVehicleValidationRules(), validate, findUsernameAndInitializeUpload ,addDriverAndVehicle);
+router.post('/driver/:id', 
+    async (req,res, next) => {
+        try {
+            findUsernameAndInitializeUpload(req, res, next,Number(req.params.id))
+            console.log("find username success");
+        } catch (error) {
+            console.log("find username failed"); 
+        }
+    },
+    addDriverAndVehicle
+);
 
 export default router;
