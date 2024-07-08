1f15275b6e487f0395fcc23261ca55534fd3a54c
George Zarokanellos <geo.zarak@gmail.com>
2024-07-08 20:05:56 +0300

    Added material UI rating library and made a global component

---
+++ frontend/carpool-app/src/util/common_functions.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Rating from '@mui/material/Rating';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+interface RatingProps {
+    rating: number;
+}
+
+export const StarRating: React.FC<RatingProps> = ({ rating }) => {
+    return (
+        <Rating name="read-only" value={rating} readOnly />
+    )
+}
