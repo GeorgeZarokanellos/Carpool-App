80dcb42d6f458d805cc211d9b9d92d54471dd4e9
George Zarokanellos <geo.zarak@gmail.com>
2024-07-08 20:07:57 +0300

    Received/Submitted reviews of the user are now displayed in the profile

--- frontend/carpool-app/src/pages/Profile.tsx
+++ frontend/carpool-app/src/pages/Profile.tsx
@@ -1,22 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import { IonContent, IonIcon, IonImg, IonItem, IonLoading, IonPage} from '@ionic/react';
+import { IonContent, IonIcon, IonImg, IonItem, IonLoading, IonPage, IonTitle} from '@ionic/react';
 import './Profile.scss';
 import instance from '../AxiosConfig';
 import { type ProfileData } from '../interfacesAndTypes/Types';
 import { star, starHalf, starOutline } from 'ionicons/icons';
+import { SubmittedReceivedReviewsDisplay } from '../components/ReviewsDisplay';
 
 const Profile: React.FC = () => {
   const [profileData, setProfileData] = useState<ProfileData>();
   const [ratingStars, setRatingStars] = useState<JSX.Element[]>([]);
+  const [imageUrl, setImageUrl] = useState<string>('');
   let rating:number;
+
+  // function bufferToBase64(buffer: Buffer){
+  //   let binary = '';
+  //   const bytes = [].slice.call(new Uint8Array(buffer));
+  //   bytes.forEach((b) => binary += String.fromCharCode(b));
+  //   return window.btoa(binary);
+  // }
 
   useEffect(() => {
     instance.get(`/profile/${localStorage.getItem('userId')}`)
     .then(response => {
       setProfileData(response.data);
-      console.log(profileData);
-      if(profileData)
-        rating = Number(profileData?.overallRating);
+      console.log("Profile data retrieved: ", profileData);
+      if(profileData){
+        rating = Number(profileData.overallRating);
+        // const url = URL.createObjectURL(profileData.profilePicture);  //create a url for the image stored in the memory
+        // setImageUrl(url);
+      }
     })
     .catch(error => {
       console.log(error);
@@ -50,16 +62,25 @@ const Profile: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        <IonContent>
-          <div>
-            <IonImg />
-            {/* <h1>{profileData.firstName} {profileData.lastName}</h1> */}
-            <IonItem lines='none'>
-              <div>
-                {ratingStars}
+        <IonContent >
+          { profileData ? (
+            <div className='profile-container'>
+              <div className='profile-contents'>
+                {/* <IonImg src={imageUrl} alt=''/> */}
+                <IonTitle>{profileData.firstName} {profileData.lastName}</IonTitle>
+                <IonItem lines='none' >
+                  {/* <div>
+                    {ratingStars}
+                  </div> */}
+                </IonItem>
+                <IonItem lines='none'>
+                    < SubmittedReceivedReviewsDisplay submittedReviews={profileData.userSubmittedReviews} userReviews={profileData.userReviews}/>
+                </IonItem>
               </div>
-            </IonItem>
-          </div>
+            </div>
+          ): (
+            <IonLoading isOpen={true} message={"Retrieving profile information.."} />
+          )}
         </IonContent>
       </IonContent>
     </IonPage>
