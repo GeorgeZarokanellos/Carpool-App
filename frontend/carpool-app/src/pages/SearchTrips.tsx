73b9327108e012662855039e51ecf85273d94aa8
George Zarokanellos <geo.zarak@gmail.com>
2024-07-11 00:32:03 +0300

    Implemented trip search functionality. Needs to be tested further

--- frontend/carpool-app/src/pages/SearchTrips.tsx
+++ frontend/carpool-app/src/pages/SearchTrips.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect} from 'react';
+import React, { useEffect, useState} from 'react';
 import { IonContent, IonGrid, IonHeader, IonPage, IonRow, IonSearchbar } from '@ionic/react';
 import { TripInformation } from '../components/TripInformation';
 import { Trip } from '../interfacesAndTypes/Types';
@@ -10,7 +10,8 @@ interface searchTripProps {
 }
 
 const SearchTrips: React.FC<searchTripProps> = (refreshKey) => {
-  const [trips, setTrips] = React.useState<Trip[]>([]);
+  const [trips, setTrips] = useState<Trip[]>([]);
+  const [filteredResults, setFilteredResults] = useState<Trip[]>([]);
   let formattedDate;
   let formattedTime;
 
@@ -20,6 +21,9 @@ const SearchTrips: React.FC<searchTripProps> = (refreshKey) => {
     .then(response => {
       console.log('response',response);
       setTrips(response.data);
+      setFilteredResults(response.data);
+      console.log("filtered data from response", filteredResults);
+      
     })
     .catch(error => {
       console.log(error);
@@ -27,18 +31,42 @@ const SearchTrips: React.FC<searchTripProps> = (refreshKey) => {
   
   },[refreshKey])
 
+  const handleSearch = (event: CustomEvent) => {
+    console.log(event.detail.value);
+    if(event.detail.value === ""){
+      console.log("Event content empty", event.detail.value);
+      setFilteredResults(trips);
+      console.log("Filtered results", filteredResults);
+      
+    }
+    else {
+      console.log(filteredResults.map((trip)=> trip.startLocation));
+      
+      console.log(filteredResults.filter(trip => trip.startLocation.toLowerCase().includes(event.detail.value.toLowerCase())));
+      setFilteredResults(trips.filter(trip => trip.startLocation.toLowerCase().includes(event.detail.value.toLowerCase())));
+      console.log("filtered results", filteredResults);
+      
+    }
+  }
 
 
 
   return (
     <IonPage>
       <IonHeader className='ion-no-border'>
-        <IonSearchbar placeholder='Search available trips' animated={true} class='custom'/>
+        <IonSearchbar 
+          placeholder='Search available trips' 
+          animated={true}
+          onIonChange={handleSearch} 
+          class='custom'/>
       </IonHeader>
       <IonContent >
         <IonGrid >
           <IonRow className='ion-justify-content-center ion-align-items-center'>
-            {trips.map((trip) => {
+            {filteredResults.map((trip) => {
+              // console.log(trip);
+              // console.log(trip.driver.user);
+              
               const date = new Date(trip.startingTime);
               formattedDate = date.toLocaleDateString();
               const timeParts = date.toLocaleTimeString().split(':');              
@@ -55,7 +83,7 @@ const SearchTrips: React.FC<searchTripProps> = (refreshKey) => {
                     noOfPassengers={trip.noOfPassengers}
                     noOfStops={trip.noOfStops}
                     finish='Πρητανεία'
-                    driver={trip.driver}
+                    driver={{user: trip.driver.user}}
                     tripCreator ={trip.tripCreator}
                     />
               )
