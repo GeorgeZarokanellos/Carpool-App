ce34232a0b4842b2ab507fc2321a33a46d324ee9
George Zarokanellos <geo.zarak@gmail.com>
2024-07-10 16:43:58 +0300

    Added formatDate and formatDateTime functions because they are used in multiple components

--- frontend/carpool-app/src/util/common_functions.tsx
+++ frontend/carpool-app/src/util/common_functions.tsx
@@ -12,3 +12,33 @@ export const StarRating: React.FC<RatingProps> = ({ rating }) => {
         <Rating name="read-only" value={rating} precision={0.5} readOnly />
     )
 }
+
+export const formatDate = (dateString: string | Date) => {
+    let dateParts: string[] = [];
+    let formattedDate: string;
+    if(dateString instanceof Date){
+        dateParts = dateString.toString().split('-');
+    } else 
+        dateParts = dateString.split('-');
+    if(dateParts.length !== 0){
+        formattedDate = `${dateParts[1]}/${dateParts[2]}`;
+        return formattedDate;
+    } else 
+        console.log("DateParts is empty:", dateParts);
+        
+}
+
+export const formatDateTime = (dateString: string) => {
+    const dateTimeParts = dateString.split('T');
+    console.log(dateTimeParts);
+    const formattedDate = formatDate(dateTimeParts[0]);
+    console.log(formattedDate);
+    const timeParts = `${dateTimeParts[1]}`.split(':');
+    console.log(timeParts);
+    const formattedTime = `${timeParts[0]}:${timeParts[1]}`;
+    
+    return {
+        formattedDate,
+        formattedTime
+    }
+}
