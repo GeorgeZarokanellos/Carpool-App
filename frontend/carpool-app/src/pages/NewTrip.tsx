408e7a3cf5700248e3cd27cd9797297562558736
George Zarokanellos <geo.zarak@gmail.com>
2024-07-16 12:00:05 +0300

    Started implementation of new trip screen

--- frontend/carpool-app/src/pages/NewTrip.tsx
+++ frontend/carpool-app/src/pages/NewTrip.tsx
@@ -1,28 +1,83 @@
-import React, { useEffect } from "react";
+import { IonAlert, IonButton, IonContent, IonFooter, IonHeader, IonInput, IonPage, IonPicker } from "@ionic/react";
+import React, { useEffect, useState } from "react";
+import instance from "../AxiosConfig";
+import { Stop } from "../interfacesAndTypes/Types";
+import { text } from "ionicons/icons";
 
 export const NewTrip: React.FC = () => {
-    let tripCreatorId: number;
-    
+    const role: string | null = localStorage.getItem('role');
+    const userIdString = localStorage.getItem('userId');
+    const [showAlert] = useState(role === 'driver');
+    const [startingLocations, setStartingLocations] = useState<Stop[]>([]);
+    const [tripDriverId, setTripDriverId] = useState<number | null>(null);
+    const [showStartingLocationPicker, setShowStartingLocationPicker] = useState(false);
+    let userIdInt: number;
+    if(userIdString)
+        userIdInt = parseInt(userIdString, 10);
+    // console.log(localStorage);
+
+    // instance.post('/trip', {
+
+    // })
+
     useEffect(() => {
-        const storedUserIdString = localStorage.getItem('userId');
-        if( storedUserIdString !== null){
-            const storedUserIdInt = parseInt(storedUserIdString, 10);
-
-            if(storedUserIdInt){
-                tripCreatorId = storedUserIdInt;
-            } else {
-                console.log("StoredUserInt doesnt exist from NewTrip component");
-                
-            }
-
-        } else {
-            console.log("Local storage is empty from NewTrip component");
-        }
-    },[]);
-
+        instance.get('/trips/starting-locations')
+        .then(response => {
+            console.log("Starting locations: ", response.data);
+            setStartingLocations(response.data);
+        })
+    },[])
+    
     return (
         <div>
-            
+            <IonPage>
+                <IonHeader>
+                    Create a new trip
+                </IonHeader>
+                <IonContent>
+                    <IonAlert 
+                        isOpen={showAlert} 
+                        header="Are you gonna be the driver or not?"
+                        buttons={[{
+                                text: 'yes',
+                                role: 'confirm',
+                                handler: () => {
+                                    setTripDriverId(userIdInt);
+                                }
+                            },
+                            {
+                                text: 'no',
+                                role: 'cancel',
+                                handler: () => {
+                                    console.log("The user is a passenger");
+                                }
+                            }
+                        ]}
+                    />
+                    <div className="new-trip-input-container">
+                        <form className="trip-form">
+                            <div className="form-contents">
+                                <IonButton onClick={() => setShowStartingLocationPicker(true)}/>
+                                <IonPicker 
+                                    isOpen={showStartingLocationPicker}
+                                    columns={[
+                                        {
+                                            name: 'Starting Locations',
+                                            options: startingLocations.map((stop, index) => ({
+                                                text: stop.stopLocation,
+                                                value: index
+                                            }))
+                                        }
+                                    ]}    
+                                />
+                            </div>
+                        </form>
+                    </div>
+                </IonContent>
+                <IonFooter>
+
+                </IonFooter>
+            </IonPage>
         </div>
     );
 }
