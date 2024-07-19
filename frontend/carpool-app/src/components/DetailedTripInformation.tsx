e248ddac54e44106da8649fb97451bbeadc333e7
George Zarokanellos <geo.zarak@gmail.com>
2024-07-20 02:03:08 +0300

    Added the get request to retrieve the detailed info about the trip and used the MapDisplay component for the map

--- frontend/carpool-app/src/components/DetailedTripInformation.tsx
+++ frontend/carpool-app/src/components/DetailedTripInformation.tsx
@@ -1,15 +1,61 @@
-import { IonContent, IonTitle } from "@ionic/react";
-import React from "react";
-import { Trip } from "../interfacesAndTypes/Types";
+import { IonContent, IonHeader, IonItem, IonPage, IonTitle } from "@ionic/react";
+import React, { useEffect, useState } from "react";
+import { ExtendedTrip, Stop, Trip } from "../interfacesAndTypes/Types";
+import { TripTitle } from "./TripTitle";
+import { formatDateTime } from "../util/common_functions";
+import instance from "../AxiosConfig";
+import { TripMapDisplay } from "./TripMapDisplay";
+
 
 interface detailedTripInfoProps {
-    tripClicked: Trip;
+    clickedTripId: number;
 }
 
-export const DetailedTripInformation: React.FC<detailedTripInfoProps> = ({ tripClicked }) => {
+export const DetailedTripInformation: React.FC<detailedTripInfoProps> = ({ clickedTripId }) => {
+
+    const viewportWidth = window.innerWidth;
+    const viewportHeight = window.innerHeight
+    const [tripData, setTripData] = useState<ExtendedTrip>();
+
+    useEffect(() => {
+        instance.get(`/trips/${clickedTripId}`)
+        .then(response => {
+            console.log(response.data.tripStops);
+            setTripData(response.data);
+            // console.log('tripData', tripData);
+            
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }, [clickedTripId]);
+
+    useEffect(() => {
+        console.log('tripData', tripData);
+    }, [tripData]);
+
     return (
-        <IonContent>
-            {/* <IonTitle>{tripClicked.}</IonTitle> */}
-        </IonContent>
+        <IonPage style={{height: `${viewportHeight}`, width: `${viewportWidth}`}}>
+            { tripData? (
+                <IonHeader>
+                    <IonTitle>
+                        <TripTitle 
+                            dateOfTrip={formatDateTime(tripData.startingTime).formattedDate} 
+                            tripCreator={tripData.tripCreator}
+                        />
+                    </IonTitle>
+                </IonHeader>
+            ) : ''}
+            { tripData ? (
+                
+                <IonContent>
+                    <TripMapDisplay tripStops={tripData.tripStops}/>
+                    <IonItem lines="none">
+                        <div></div>
+                    </IonItem>
+                </IonContent>
+            
+            ): ''}
+        </IonPage>
     )
 }
