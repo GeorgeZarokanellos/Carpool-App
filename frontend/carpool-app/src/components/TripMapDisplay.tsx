c8ab06243c5bc5c49b2dc75336f2f72288838ead
George Zarokanellos <geo.zarak@gmail.com>
2024-07-20 01:59:12 +0300

    Seperate component for displaying the map in the detailed view of a trip

---
+++ frontend/carpool-app/src/components/TripMapDisplay.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { MapContainer , TileLayer, Marker, Polyline, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { ExtendedTrip, Stop, TripStops } from '../interfacesAndTypes/Types';
+import { LatLngExpression } from 'leaflet';
+
+interface TripMapDisplayProps {
+    tripStops: TripStops;
+}
+
+export const TripMapDisplay: React.FC<TripMapDisplayProps> = ({tripStops}) => { 
+    console.log(tripStops);
+    const startPosition: LatLngExpression = [38.24368476508384, 21.73212381808353]; 
+    const endPosition: LatLngExpression = [38.28623463741879, 21.785996514033958]; //prutaneia    
+    const stops: LatLngExpression[] = tripStops.map(stop => [stop.details.lat, stop.details.lng]);
+    console.log(stops);
+    
+    return (
+        <MapContainer center={[38.24368476508384, 21.73212381808353]} zoom={14} style={{height: '60vh', width: '100%'}}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {stops.map((position, index) => (
+                console.log();
+                
+                <Marker key={index} position={position} />
+            ))}
+            <Polyline positions={[startPosition, ...stops, endPosition]} color='blue'/>
+        </MapContainer>
+    )
+}
