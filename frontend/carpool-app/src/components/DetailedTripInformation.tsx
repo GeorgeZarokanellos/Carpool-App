889c3716f4d30e7f680237f28597130d55101770
GeorgeZarokanellos <geo.zarak@gmail.com>
2024-07-22 17:23:37 +0300

    Added two columns on for the trip passengers and driver and the other for the trip and stops

--- frontend/carpool-app/src/components/DetailedTripInformation.tsx
+++ frontend/carpool-app/src/components/DetailedTripInformation.tsx
@@ -1,10 +1,13 @@
-import { IonContent, IonHeader, IonItem, IonPage, IonTitle } from "@ionic/react";
+import { IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonItem, IonLabel, IonPage, IonRow, IonTitle } from "@ionic/react";
 import React, { useEffect, useState } from "react";
 import { ExtendedTrip, Stop, Trip } from "../interfacesAndTypes/Types";
 import { TripTitle } from "./TripTitle";
 import { formatDateTime } from "../util/common_functions";
 import instance from "../AxiosConfig";
 import { TripMapDisplay } from "./TripMapDisplay";
+import './DetailedTripInformation.scss';
+import { peopleOutline, timeOutline } from "ionicons/icons";
+import { PassengersDetails } from "./PassengersDetails";
 
 
 interface detailedTripInfoProps {
@@ -50,9 +53,34 @@ export const DetailedTripInformation: React.FC<detailedTripInfoProps> = ({ click
                 
                 <IonContent>
                     <TripMapDisplay tripStops={tripData.tripStops}/>
-                    <IonItem lines="none">
-                        <div></div>
-                    </IonItem>
+                    <div className="grid-contents">
+                        <IonTitle class="ion-text-center">Πληροφορίες ταξιδιού</IonTitle>
+                        <IonGrid>    
+                            <IonRow>
+                                <IonCol size="6" className="custom-col">
+                                    <div className="passenger-time-info">
+                                        <IonItem lines="none" >
+                                            <IonIcon icon={timeOutline} slot="start" className="time-icon" />
+                                            <IonLabel>{formatDateTime(tripData.startingTime).formattedTime}</IonLabel>
+                                        </IonItem>
+                                        <IonItem lines="none">
+                                            <IonIcon icon={peopleOutline} slot="start" className="people-icon" />
+                                            <IonLabel>{tripData.noOfPassengers + ' συνεπιβάτες'} </IonLabel>
+                                        </IonItem>
+                                        <div className="passengers-details">
+                                            <PassengersDetails passengers={tripData.tripPassengers}/>
+                                        </div>
+                                    </div>
+                                </IonCol>
+                                <IonCol size="6" className="custom-col">
+                                    <IonItem lines="none">
+                                        <h2>Προορισμός</h2>
+                                        <p>Πρυτανεία</p>
+                                    </IonItem>
+                                </IonCol>
+                            </IonRow>
+                        </IonGrid>
+                    </div>
                 </IonContent>
             
             ): ''}
