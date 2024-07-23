52be92cb750bdc8c99a808223a81960bd238ba6b
GeorgeZarokanellos <geo.zarak@gmail.com>
2024-07-23 16:33:09 +0300

    Added rating display for each passenger

--- frontend/carpool-app/src/components/PassengersDetails.tsx
+++ frontend/carpool-app/src/components/PassengersDetails.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { tripPassenger } from "../interfacesAndTypes/Types";
 import { IonAvatar, IonIcon, IonImg, IonItem, IonLabel } from "@ionic/react";
 import './PassengersDetails.scss';
-import { arrayBufferTo64String } from "../util/common_functions";
+import { arrayBufferTo64String, StarRating } from "../util/common_functions";
 
 interface PassengersDetailsProps{
     passengers: tripPassenger[];
@@ -11,19 +11,22 @@ interface PassengersDetailsProps{
 export const PassengersDetails: React.FC<PassengersDetailsProps> = ({passengers}) => {
     console.log("Passengers: ", passengers);
     // console.log("Passenger profile picture: ", arrayBufferTo64String(passengers[0].passenger.profilePicture));
-    
     return (
         <div className="passenger-item">
             {passengers.map((passenger,index) => {
+                const passengerRating = parseFloat(passenger.passenger.overallRating);
                 return (
-                    <IonItem key={index} lines="none">
-                        <IonAvatar >
-                                <img  
-                                src={passenger.passenger.profilePicture? arrayBufferTo64String(passenger.passenger.profilePicture) : "https://ionicframework.com/docs/img/demos/avatar.svg" }
-                                style={{width: '100%', height: '100%', objectFit: 'cover'}}
-                            />
-                        </IonAvatar>
-                        <IonLabel >{passenger.passenger.firstName + ' ' + passenger.passenger.lastName}</IonLabel>
+                    <IonItem key={index} lines="none" >
+                            <IonAvatar style={{marginRight: '1rem'}}>
+                                    <img  
+                                    src={passenger.passenger.profilePicture? arrayBufferTo64String(passenger.passenger.profilePicture) : "https://ionicframework.com/docs/img/demos/avatar.svg" }
+                                    style={{width: '100%', height: '100%', objectFit: 'cover'}}
+                                />
+                            </IonAvatar>
+                            <div style={{display: 'flex', flexDirection: 'column'}}>
+                                <IonLabel >{passenger.passenger.firstName + ' ' + passenger.passenger.lastName}</IonLabel>
+                                <StarRating rating={passengerRating} />
+                            </div>
                     </IonItem>
                 );
             })}
