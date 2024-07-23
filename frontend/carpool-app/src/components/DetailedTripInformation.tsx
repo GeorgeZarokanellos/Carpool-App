da9f61638052a11bc4b93e88adf4665ce9e22f24
GeorgeZarokanellos <geo.zarak@gmail.com>
2024-07-23 16:32:42 +0300

    All of the trip stops are now displayed as well as some driver info

--- frontend/carpool-app/src/components/DetailedTripInformation.tsx
+++ frontend/carpool-app/src/components/DetailedTripInformation.tsx
@@ -1,14 +1,21 @@
-import { IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonItem, IonLabel, IonPage, IonRow, IonTitle } from "@ionic/react";
+import { IonAvatar, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonItem, IonLabel, IonPage, IonRow, IonTitle } from "@ionic/react";
 import React, { useEffect, useState } from "react";
-import { ExtendedTrip, Stop, Trip } from "../interfacesAndTypes/Types";
+import { ExtendedTrip, Stop, Trip, TripStops } from "../interfacesAndTypes/Types";
 import { TripTitle } from "./TripTitle";
 import { formatDateTime } from "../util/common_functions";
 import instance from "../AxiosConfig";
 import { TripMapDisplay } from "./TripMapDisplay";
 import './DetailedTripInformation.scss';
-import { peopleOutline, timeOutline } from "ionicons/icons";
 import { PassengersDetails } from "./PassengersDetails";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { arrayBufferTo64String, StarRating } from "../util/common_functions";
 
+//icons
+import { carOutline, peopleOutline, timeOutline } from "ionicons/icons";
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import HailIcon from '@mui/icons-material/Hail';
+
+//TODO make the right column scrollable
 
 interface detailedTripInfoProps {
     clickedTripId: number;
@@ -33,13 +40,13 @@ export const DetailedTripInformation: React.FC<detailedTripInfoProps> = ({ click
         })
     }, [clickedTripId]);
 
-    useEffect(() => {
-        console.log('tripData', tripData);
-    }, [tripData]);
+    // useEffect(() => {
+    //     console.log('tripData', tripData);
+    // }, [tripData]);
 
     return (
         <IonPage style={{height: `${viewportHeight}`, width: `${viewportWidth}`}}>
-            { tripData? (
+            {/* { tripData? (
                 <IonHeader>
                     <IonTitle>
                         <TripTitle 
@@ -48,7 +55,7 @@ export const DetailedTripInformation: React.FC<detailedTripInfoProps> = ({ click
                         />
                     </IonTitle>
                 </IonHeader>
-            ) : ''}
+            ) : ''} */}
             { tripData ? (
                 
                 <IonContent>
@@ -57,7 +64,7 @@ export const DetailedTripInformation: React.FC<detailedTripInfoProps> = ({ click
                         <IonTitle class="ion-text-center">Πληροφορίες ταξιδιού</IonTitle>
                         <IonGrid>    
                             <IonRow>
-                                <IonCol size="6" className="custom-col">
+                                <IonCol size="7" className="custom-col">
                                     <div className="passenger-time-info">
                                         <IonItem lines="none" >
                                             <IonIcon icon={timeOutline} slot="start" className="time-icon" />
@@ -70,13 +77,58 @@ export const DetailedTripInformation: React.FC<detailedTripInfoProps> = ({ click
                                         <div className="passengers-details">
                                             <PassengersDetails passengers={tripData.tripPassengers}/>
                                         </div>
+                                        <div className="driver-details">
+                                            <IonItem lines="none" >
+                                                <IonIcon icon={carOutline} slot="start" className="car-icon" />
+                                                <IonLabel>Οδηγός</IonLabel>
+                                            </IonItem>
+                                            <IonItem lines="none">
+                                                <IonAvatar style={{marginRight: '1rem', width: "50%"}}>
+                                                    <img  
+                                                        src={tripData.driver? arrayBufferTo64String(tripData.driver.user.profilePicture) : "https://ionicframework.com/docs/img/demos/avatar.svg" }
+                                                        style={{width: '100%', height: '100%', objectFit: 'cover'}}
+                                                    />
+                                                </IonAvatar>
+                                                <div style={{display: "flex", flexDirection: "column"}}>
+                                                    <IonLabel>{tripData.driver?.user.firstName + ' ' + tripData.driver?.user.lastName}</IonLabel>
+                                                    <StarRating rating={Number(tripData.driver?.user.overallRating) } />
+                                                </div>
+                                            </IonItem>
+                                        </div>
                                     </div>
                                 </IonCol>
-                                <IonCol size="6" className="custom-col">
-                                    <IonItem lines="none">
-                                        <h2>Προορισμός</h2>
-                                        <p>Πρυτανεία</p>
-                                    </IonItem>
+                                <IonCol size="5" className="custom-col">
+                                    {/* <Swiper direction="vertical" freeMode={true} scrollbar={true} style={{height: '100%'}}> */}
+                                        {/* <SwiperSlide> */}
+                                            <div className="stops-display" style={{ overflowY: 'auto', maxHeight: '100%' }}>
+                                                <div className="stop">
+                                                    <LocationOnIcon />
+                                                    {tripData.startLocation}
+                                                </div>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="80px" viewBox="6 6 20 20">
+                                                    <g transform="scale(1.4)">{/* used to scale only the arrow and not the rectangle around it */}
+                                                        <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="m8 18l4 4m0 0l4-4m-4 4V2"/>
+                                                    </g>
+                                                </svg>
+                                                {tripData.tripStops.map((stop, index) => {
+                                                    return (
+                                                        <>
+                                                            <div key={index} className="stop">
+                                                                <HailIcon />
+                                                                {stop.details.stopLocation}
+                                                            </div>
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="80px" viewBox="6 6 20 20">
+                                                                <g transform="scale(1.4)">{/* used to scale only the arrow and not the rectangle around it */}
+                                                                    <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="m8 18l4 4m0 0l4-4m-4 4V2"/>
+                                                                </g>
+                                                            </svg>
+                                                        </>
+                                                    );
+                                                })}
+                                                Πρυτανεία
+                                            </div>
+                                        {/* </SwiperSlide> */}
+                                    {/* </Swiper> */}
                                 </IonCol>
                             </IonRow>
                         </IonGrid>
