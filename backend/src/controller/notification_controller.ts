337f773882e8f2ae36225dc2469ae66fe8c1e7fd
George Zarokanellos <geo.zarak@gmail.com>
2024-07-29 18:32:19 +0300

    Now the promises are wrapped by a try catch and responses are sent after succesful completion

--- backend/src/controller/notification_controller.ts
+++ backend/src/controller/notification_controller.ts
@@ -2,11 +2,20 @@ import { type NextFunction, type Request, type Response } from 'express';
 import sequelize from '../database/connect_to_db';
 import Notification from '../model/notification';
 import { Transaction } from 'sequelize';
-import { notificationInterface } from '../interface/interface';
+import { notificationInterface, updatedNotificationInterface } from '../interface/interface';
+import { Op } from 'sequelize';
 
 export const getNotifications = async(req: Request, res: Response, next: NextFunction) => {
     try {
-        const notifications = await Notification.findAll();
+        const userId: string = req.params.userId;
+        const notifications = await Notification.findAll({
+            where: {
+                [Op.or]: [
+                    {driverId: userId},
+                    {passengerId: userId}
+                ]
+            }
+        });
         res.status(200).json(notifications);
     } catch (error) {
         console.error(error);
@@ -40,27 +49,68 @@ export const getNotification = async(req: Request, res: Response, next: NextFunc
 }
 
 export const createNotification = async(req: Request, res: Response, next: NextFunction) => {
-    await sequelize.transaction(async (transaction: Transaction) => {
-        const {receiverId, message}: notificationInterface = req.body;
-        const notification = await Notification.create({receiverId, message});
-        await notification.save({transaction});
-    }).catch((err) => {
+    try {
+        await sequelize.transaction(async (transaction: Transaction) => {
+            const {driverId, passengerId, tripId, message, stopId}: notificationInterface = req.body;
+            const notification = await Notification.create({driverId, passengerId, tripId, message, stopId});
+            await notification.save({transaction}); 
+        });
+        res.status(201).send('Notification created');  
+    } catch (err) {
         console.error(err);
-        if(typeof err === 'string'){
+        if (typeof err === 'string') {
             console.log("There was an error creating the notification: " + err);
             res.status(500).send('Error creating notification: ' + err);
-        } else if (err instanceof Error){
-            console.log(err.message); 
+        } else if (err instanceof Error) {
+            console.log(err.message);
             res.status(500).send('Error creating notification: ' + err.message);
         }
-    });
+    }
 }
 
 export const updateNotification = async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        await sequelize.transaction(async (transaction: Transaction) => {
+            const notificationId: string = req.params.notificationId;
+            const updateDetails: updatedNotificationInterface = req.body;
+            const notification = await Notification.findByPk(notificationId);
+            if(notification !== null){
+                await notification.update(updateDetails, {transaction});
+                await notification.save({transaction});
+            } else {
+                res.status(404).send('Notification not found');
+            }
+        });
+        res.status(200).send('Notification updated');
+    } catch (error) {
+        console.error(error);
+        if(typeof error === 'string'){
+            // console.log("There was an error updating the trip: " + error);
+            res.status(500).send('Error updating notification: ' + error);
+        } else if (error instanceof Error){
+            console.log(error.message); 
+            res.status(500).send('Error updating notification: ' + error.message);
+        }
+    }
+}
+
+export const deleteNotification = async(req: Request, res: Response, next: NextFunction) => {
     await sequelize.transaction(async (transaction: Transaction) => {
         const notificationId: string = req.params.notificationId;
-        const userId: number = req.body;
         const notification = await Notification.findByPk(notificationId);
-        const updatedStatus: string = req.body;
+        if(notification !== null){
+            await notification.destroy({transaction});
+        } else {
+            res.status(404).send('Notification not found');
+        }
+    }).catch((err) => {
+        console.error(err);
+        if(typeof err === 'string'){
+            console.log("There was an error deleting the notification: " + err);
+            res.status(500).send('Error deleting notification: ' + err);
+        } else if (err instanceof Error){
+            console.log(err.message); 
+            res.status(500).send('Error deleting notification: ' + err.message);
+        }
     });
 }
