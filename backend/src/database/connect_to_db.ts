b3a0ed12a98b64fa5c7324cadab6c6a38ae770f4
George Zarokanellos <geo.zarak@gmail.com>
2024-07-30 20:18:06 +0300

    Specified the timezone for correct timestamp in notifications

--- backend/src/database/connect_to_db.ts
+++ backend/src/database/connect_to_db.ts
@@ -5,7 +5,8 @@ import { env } from '../config';
 const sequelize = new  Sequelize(env.DB_NAME, env.DB_USER, env.DB_PASSWORD, {
     host: env.DB_HOST,
     port: env.DB_PORT,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    timezone: 'Europe/Athens',
 })
 
 sequelize.authenticate()
