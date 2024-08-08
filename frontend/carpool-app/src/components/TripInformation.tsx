f98cdf8570afc93f92b5b503fc1e92fe409445a5
George Zarokanellos <geo.zarak@gmail.com>
2024-08-08 21:00:42 +0300

    Removed the arrow icon and changed how the driver information is being displayed

--- frontend/carpool-app/src/components/TripInformation.tsx
+++ frontend/carpool-app/src/components/TripInformation.tsx
@@ -18,7 +18,8 @@ import {
         peopleOutline, 
         locationOutline, 
         flagOutline,
-        car,
+        carOutline,
+        starOutline,
     } from 'ionicons/icons';
 import './TripInformation.scss';
 import { StarRating } from "../util/common_functions";
@@ -50,12 +51,9 @@ export const TripInformation: React.FC<TripProps>  = ({startingTime, dateOfTrip,
                                 </div>
                             </IonCol>
                             <IonCol size="2">
-                                <div className="arrow-container">
-                                    {/* <IonItem lines="none"> */}
-                                        {/* <IonIcon icon={arrowForward} className="arrow-icon"/> */}
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="100px" height="30px" viewBox="0 0 16 16"><path fill="black" fillRule="evenodd" d="M10.159 10.72a.75.75 0 1 0 1.06 1.06l3.25-3.25L15 8l-.53-.53l-3.25-3.25a.75.75 0 0 0-1.061 1.06l1.97 1.97H1.75a.75.75 0 1 0 0 1.5h10.379z" clipRule="evenodd"></path></svg>
-                                    {/* </IonItem> */}
-                                </div>
+                                {/* <div className="arrow-container"> */}
+                                        {/* <svg xmlns="http://www.w3.org/2000/svg" width="100px" height="30px" viewBox="0 0 16 16"><path fill="black" fillRule="evenodd" d="M10.159 10.72a.75.75 0 1 0 1.06 1.06l3.25-3.25L15 8l-.53-.53l-3.25-3.25a.75.75 0 0 0-1.061 1.06l1.97 1.97H1.75a.75.75 0 1 0 0 1.5h10.379z" clipRule="evenodd"></path></svg> */}
+                                {/* </div> */}
                             </IonCol>
                             <IonCol size="5">
                                 <div className="column-contents">
@@ -87,13 +85,13 @@ export const TripInformation: React.FC<TripProps>  = ({startingTime, dateOfTrip,
                         <IonCol size="6" className="">
                             <div className="driver-info-container">
                                 <IonItem lines="none" color={itemColor}>
-                                    <div className="position-end">  {/* TODO: Fix the position of this div to the end of the container */}
-                                        <IonIcon icon={car} className="car-icon" />
-                                        <IonLabel>{(driver? driver.user.firstName + ' ' + driver.user.lastName : 'No driver yet')}</IonLabel>
+                                    <div className="position-end">  
+                                        <IonLabel >{(driver? driver.user.firstName + ' ' + driver.user.lastName : 'No driver yet')}</IonLabel>
                                     </div>
                                 </IonItem>
                                 <IonItem lines="none" color={itemColor}>
                                     <div className="position-end">
+                                        <IonIcon icon={carOutline} className="car-icon" />
                                         < StarRating rating={Number(driver.user.overallRating)}/>
                                     </div>
                                 </IonItem>
