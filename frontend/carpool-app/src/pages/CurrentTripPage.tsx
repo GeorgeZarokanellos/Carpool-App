3ab148cdd3475d8cde12f2baa94b4f82a9f844ca
George Zarokanellos <geo.zarak@gmail.com>
2024-08-10 19:23:26 +0300

    Added pages to display the current trip that a user is in and the detailed version of a trip's info

---
+++ frontend/carpool-app/src/pages/CurrentTripPage.tsx
@@ -0,0 +1,48 @@
+import { IonPage } from "@ionic/react";
+import React, { useEffect } from "react";
+import { DetailedTripInformation } from "../components/DetailedTripInformation";
+import instance from "../AxiosConfig";
+
+interface CurrentTripPageProps {
+    refreshKey: number;
+}
+
+export const CurrentTripPage: React.FC<CurrentTripPageProps> = ({refreshKey}) => {
+    
+    const userId = localStorage.getItem('userId');
+    const userIdInt = Number(userId);
+    const [currentTripId, setCurrentTripId] = React.useState<number>(0);
+    const viewportWidth = window.innerWidth;
+    const viewportHeight = window.innerHeight;      
+
+    const retrieveCurrentTripId = async () => {
+        await instance.get(`/user/${userIdInt}`)
+        .then((response) => {
+            console.log("response", response.data);
+            setCurrentTripId(response.data.currentTripId);
+            
+        })
+        .catch((error) => {
+            console.log("Error retrieving current trip id", error);
+        });
+    }
+
+    useEffect(() => {
+        if(userIdInt){
+            retrieveCurrentTripId();
+        }
+    }, [refreshKey]);
+    
+    // if(currentTripId === 0){
+    //     return (
+    //         <IonPage style={{ height: `${viewportHeight}`, width: `${viewportWidth}` }}>
+    //             <h1> No current trip </h1>
+    //         </IonPage>
+    //     )
+    // }
+    return (
+        <IonPage style={{ height: `${viewportHeight}`, width: `${viewportWidth}` }}>
+            <DetailedTripInformation clickedTripId={Number(currentTripId)} />    
+        </IonPage>
+    )
+}
---
+++ frontend/carpool-app/src/pages/DetailedTripInformationPage.tsx
@@ -0,0 +1,17 @@
+import { IonPage } from "@ionic/react";
+import React from "react";
+import { useParams } from "react-router";
+import { DetailedTripInformation } from "../components/DetailedTripInformation";
+
+export const DetailedTripInformationPage: React.FC = () => {
+    
+    const {tripId} = useParams<{tripId: string}>();
+    const tripIdNumber = Number(tripId);
+    const viewportWidth = window.innerWidth;
+    const viewportHeight = window.innerHeight;
+    return (
+        <IonPage style={{ height: `${viewportHeight}`, width: `${viewportWidth}` }}>
+            <DetailedTripInformation clickedTripId={tripIdNumber} />
+        </IonPage>
+    )
+}
