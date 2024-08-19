ed868fd9e9ace8ba1b14f8a2f6ce957e03bb1b80
GeorgeZarokanellos <geo.zarak@gmail.com>
2024-08-19 16:17:28 +0300

    now checks if the request for updating the trip status has already been made once in order to not be repeated

--- frontend/carpool-app/src/components/TripInProgress.tsx
+++ frontend/carpool-app/src/components/TripInProgress.tsx
@@ -1,22 +1,34 @@
 import React, { useEffect, useState } from "react";
 import { IonButton, IonText } from "@ionic/react";
 import "./TripInProgress.scss";
+import instance from "../AxiosConfig";
 
 interface TripInProgressProps {
+    tripId: number;
     startingTime: string;
     tripDriverCurrentUser: boolean;
     setDriverWantsToEndTrip: (value: boolean) => void;
 }
 
-export const TripInProgress: React.FC<TripInProgressProps> = ({startingTime, tripDriverCurrentUser, setDriverWantsToEndTrip}) => {
+export const TripInProgress: React.FC<TripInProgressProps> = ({tripId, startingTime, tripDriverCurrentUser, setDriverWantsToEndTrip}) => {
     const [tripInProgress, setTripInProgress] = useState(false);
     useEffect(() => {
         const currentTime = new Date().getTime();                
         const tripStartTime = new Date(startingTime).getTime();
         
         //the check happens in UTC time
-        if (tripStartTime <= currentTime) {
+        if (tripStartTime <= currentTime && !tripInProgress) {
             setTripInProgress(true);
+            //update trip status to in_progress
+            instance.patch(`/trips/${tripId}`,
+                {
+                    status: "in_progress"
+                }
+            ).then(response => {
+                console.log(response);
+            }).catch(error => {
+                console.log(error);
+            });
         }
     }, [startingTime]);
     return (
