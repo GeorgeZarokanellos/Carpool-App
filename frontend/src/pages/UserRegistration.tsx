22b76af1fc1bf90d6ff1980129ec715e649cc4e0
George Zarokanellos <geo.zarak@gmail.com>
2024-10-06 20:05:14 +0300

    Used react-hook-form and yup in order for every input field in user registration to have validation and according error message

--- frontend/src/pages/UserRegistration.tsx
+++ frontend/src/pages/UserRegistration.tsx
@@ -1,19 +1,30 @@
 import React, { useEffect, useState } from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonCheckbox, IonFooter, IonAvatar, IonAlert } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonCheckbox, IonFooter, IonAvatar, IonAlert, IonLoading } from '@ionic/react';
 import './UserRegistration.scss';
 import { LabelInput } from '../components/LabelInput';
 import instance from '../AxiosConfig';
 import { useHistory } from 'react-router';
+import { useForm, Controller } from 'react-hook-form';
+import { TextFieldTypes } from '../interfacesAndTypes/Types';
+import { object, string } from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
 //TODO add tooltip for profile image
 
+type inputLabelNames = "universityId" | "firstName" | "lastName" | "username" | "password" | "email" | "phone";
+interface UserRegistrationBody {
+  universityId: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  email: string;
+  phone: string;
+  role?: string;
+  profilePicture?: Blob;
+}
+
 export const UserRegistration: React.FC = () => {
-  const [universityId, setUniversityId] = useState<number>();
-  const [firstName, setFirstName] = useState<string>('');
-  const [lastName, setLastName] = useState<string>('');
-  const [username, setUsername] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [email, setEmail] = useState<string>('');
-  const [phone, setPhone] = useState<string>('');
   const [profilePicture, setProfilePicture] = useState<Blob>();
   const [isDriver, setIsDriver] = useState(false);
   //alerts
@@ -24,30 +35,40 @@ export const UserRegistration: React.FC = () => {
   const [errorMessage, setErrorMessage] = useState('');
   const [errorAlert, setErrorAlert] = useState(false);
   const [userId, setUserId] = useState<number>();
-  // const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const history = useHistory();
   const viewportWidth = window.innerWidth;
   const viewportHeight = window.innerHeight;
-  
 
-  const handleRegistration = (e: React.FormEvent) => {
-    e.preventDefault();
+  const requestBodyValidationSchema = object().shape({
+    universityId: string().required('University ID is required').matches(/^[0-9]{12}$/, 'University Id needs to be exactly 12 characters long'),
+    firstName: string().required('First name is required').max(50, 'First name cannot exceed 50 characters'),
+    lastName: string().required('Last name is required').max(50, 'Last name cannot exceed 50 characters'),
+    username: string().required('Username is required').max(50, 'Username cannot exceed 50 characters'),
+    password: string().required('Password is required').min(3, 'Password cant be less than 3 characters').max(20, 'Password cannot exceed 20 characters').matches(/^[0-9A-Za-z!@#$%^&*_+]{3,20}$/, 'Password must be between 8 and 20 characters long'),
+    email: string().required('Email is required').matches(/^[0-9A-Za-z._+]+@[0-9A-Za-z]+\.[a-zA-Z]{2,}$/, 'Email is invalid'),
+    phone: string().required('Phone number is required').matches(/^\d{10}$/, 'Phone number must be 10 digits long')
+  });
+  const { control, handleSubmit, formState: {errors} } = useForm({
+    resolver: yupResolver(requestBodyValidationSchema),
+  });
 
+  const handleRegistration = async (data: UserRegistrationBody) => {
+    setIsLoading(true);
     const formData = new FormData();
     const userRegistrationRequestBody = {
-      universityId: universityId,
-      firstName: firstName,
-      lastName: lastName,
-      username: username,
-      password: password,
-      email: email,
-      phone: phone,
+      universityId: data.universityId,
+      firstName: data.firstName,
+      lastName: data.lastName,
+      username: data.username,
+      password: data.password,
+      email: data.email,
+      phone: data.phone,
       role: (isDriver? 'driver' : 'passenger'),
       profilePicture 
     }
 
     console.log("user registration request body", userRegistrationRequestBody);
-    
 
     //loop through the string/number entries of the object and append the the formData var
     Object.entries(userRegistrationRequestBody).forEach(([key, value])=>{
@@ -69,19 +90,31 @@ export const UserRegistration: React.FC = () => {
         console.log("response",response);
         setUserId(response.data.userId);
         setDriverCreationMessage('You have successfully registered as a user. Please fill out the driver registration form.');
-        // setIsLoading(true);
+        setIsLoading(false);
       }
       else{
         setUserCreationMessage('You have successfully registered as a user. Please log in.');
         setShowUserCreationAlert(true);
+        setIsLoading(false);
       }
     }).catch((error) => {
       console.log(error);
-      setErrorMessage('An error occurred while registering. Please try again.' + error.message);
+      setIsLoading(false);
+      setErrorMessage('An error occurred while registering. Please try again.');
       setErrorAlert(true);
     });
     
   };
+
+  const inputFieldProperties: { label: string, type: TextFieldTypes, placeholder?: string, name: inputLabelNames}[] = [
+    { label: 'University ID', type: 'number' as TextFieldTypes, placeholder: '12 digit registration number', name: 'universityId' },
+    { label: 'First Name',  type: 'text' as TextFieldTypes, name: "firstName"},
+    { label: 'Last Name', type: 'text' as TextFieldTypes, name: 'lastName'},
+    { label: 'Username', type: 'text' as TextFieldTypes, name: 'username'},
+    { label: 'Password', type: 'password' as TextFieldTypes, name: 'password'},
+    { label: 'Email', type: 'email' as TextFieldTypes, placeholder: 'example@gmail.com' , name: 'email'},
+    { label: 'Phone',  type: 'tel' as TextFieldTypes, placeholder: '10 digit personal number' , name: 'phone'},
+  ];
 
   useEffect(() => {
     if(userId !== undefined){
@@ -97,9 +130,9 @@ export const UserRegistration: React.FC = () => {
           <IonTitle style={{textAlign: 'center'}}>Registration</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
-        {/* <div className='registration-container'> */}
-            <form onSubmit={handleRegistration} id='userCredentials' className='custom-form' encType='multipart/form-data'>
+      <IonContent >
+        {/* <div className='user-registration-container'> */}
+            <form onSubmit={handleSubmit(handleRegistration)} id='userCredentials' className='custom-form' encType='multipart/form-data'>
               <div className='form-contents'>
                   <div className='profile-picture-container'>
                     {/* <IonLabel>Insert profile <br /> picture below</IonLabel> */}
@@ -110,13 +143,28 @@ export const UserRegistration: React.FC = () => {
                       <input type='file' id='profilePicture' hidden required accept='image/*' onChange={e => setProfilePicture(e.target.files?.[0])} />
                     </IonButton>
                   </div>  
-                  <LabelInput label='University ID' placeholder='12 digit registration number' value={universityId ?? ''} type='number' onIonChange={value => setUniversityId(Number(value))} />
-                  <LabelInput label='First Name' placeholder='' value={firstName} type='text' onIonChange={value => setFirstName(String(value))} />
-                  <LabelInput label='Last Name' placeholder='' value={lastName} type='text' onIonChange={value => setLastName(String(value))} />
-                  <LabelInput label='Username' placeholder='' value={username} type='text' onIonChange={value => setUsername(String(value))} />
-                  <LabelInput label='Password' placeholder='' value={password} type='password' onIonChange={value => setPassword(String(value))} />
-                  <LabelInput label='Email' placeholder='example@gmail.com' value={email} type='email' onIonChange={value => setEmail(String(value))} />
-                  <LabelInput label='Phone' placeholder='10 digit personal number' value={phone} type='tel' onIonChange={value => setPhone(String(value))} />
+                  {
+                    inputFieldProperties.map((input_field, index) => {
+                      return (
+                        <div key={index} className='controlled-input-field'>
+                            <Controller 
+                              name={input_field.name}
+                              control={control}
+                              render={({ field: {onChange, name} }) => (
+                                <LabelInput 
+                                  label={input_field.label}
+                                  placeholder={input_field.placeholder}
+                                  name={name}
+                                  type={input_field.type}
+                                  onIonChange={onChange}
+                                />
+                              )}
+                            />
+                            {errors[input_field.name] != undefined && <p className='error-message'>{errors[input_field.name]?.message}</p>}
+                        </div>
+                      )
+                    })
+                  }
                   <IonCheckbox labelPlacement='stacked' alignment='center' onIonChange={e => setIsDriver(e.detail.checked)}>
                     <span style={{fontSize:'1.3rem'}}>Wanna register as a driver?</span>
                   </IonCheckbox>
@@ -131,6 +179,10 @@ export const UserRegistration: React.FC = () => {
           </div>
         </IonToolbar>
       </IonFooter>
+      <IonLoading 
+        isOpen={isLoading}
+        message={'Please wait while the information you provided is being processed...'}
+      />
       <IonAlert 
         isOpen={showUserCreationAlert} 
         onDidDismiss={() => {
