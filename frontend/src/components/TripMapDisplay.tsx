f061c1256b0cba06fea894f8204c3ab863c4f9c7
George Zarokanellos <geo.zarak@gmail.com>
2024-10-07 18:47:12 +0300

    Added a timeout in the rendering of the map in order to be displayed correctly when the page loads

--- frontend/src/components/TripMapDisplay.tsx
+++ frontend/src/components/TripMapDisplay.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { MapContainer , TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { Stop, TripStops } from '../interfacesAndTypes/Types';
 import { LatLngExpression } from 'leaflet';
 import L from 'leaflet';
 import './TripMapDisplay.scss';
+import { useState } from 'react';
 
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
@@ -31,25 +32,32 @@ L.Marker.prototype.options.icon = DefaultIcon;
 
 export const TripMapDisplay: React.FC<TripMapDisplayProps> = ({tripStops, startLocation, endLocation}) => {   
     
+    const [renderMap, setRenderMap] = useState(false);
+
     const stops: StopDetails[] = tripStops.map(stop => ({
         coordinates: [stop.details.lat, stop.details.lng],
         location: stop.details.stopLocation,
     }));
 
+    //delay rendering of map for the map to be displayed correctly
+    useLayoutEffect(() => { // runs before the browser renders the page
+        setTimeout(() => setRenderMap(true),10);
+    }, []);
+
     return (
-        <MapContainer 
-            center={[startLocation.lat, startLocation.lng]} 
-            zoom={14} 
-            className='map' 
-            scrollWheelZoom={false}
-            >
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            {
-                stops.map((stop, index) => {
-                    return(
+        <>
+            {renderMap && (
+                <MapContainer 
+                    center={[startLocation.lat, startLocation.lng]} 
+                    zoom={14} 
+                    className='map' 
+                    scrollWheelZoom={false}
+                >
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    {stops.map((stop, index) => (
                         <Marker 
                             key={index} 
                             position={stop.coordinates} 
@@ -58,24 +66,24 @@ export const TripMapDisplay: React.FC<TripMapDisplayProps> = ({tripStops, startL
                                     console.log(`Marker ${index} clicked`); 
                                 },
                             }}
-                            >
-                            <Popup >
+                        >
+                            <Popup>
                                 {stop.location}
                             </Popup>
                         </Marker>
-                    )
-                } 
+                    ))}
+                    <Marker position={[startLocation.lat, startLocation.lng]}>
+                        <Popup>
+                            {"Start " + startLocation.stopLocation}
+                        </Popup>
+                    </Marker>
+                    <Marker position={[endLocation.lat, endLocation.lng]}>
+                        <Popup>
+                            {"Finish " + endLocation.stopLocation}
+                        </Popup>
+                    </Marker>
+                </MapContainer>
             )}
-            <Marker position={[startLocation.lat, startLocation.lng]}>
-                <Popup>
-                    {"Start " + startLocation.stopLocation}
-                </Popup>
-            </Marker>
-            <Marker position={[endLocation.lat, endLocation.lng]}>
-                <Popup>
-                    {"Finish " + endLocation.stopLocation}
-                </Popup>
-            </Marker>
-        </MapContainer>
-    )
+        </>
+    );
 }
