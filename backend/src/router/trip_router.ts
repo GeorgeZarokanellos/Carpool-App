991cfdb7588d8f987c8ba2464f9370a1f78fb6fa
George Zarokanellos <geo.zarak@gmail.com>
2024-10-21 17:59:04 +0300

    Returns the status of the trip whose id is specified in order to check if its cancelled

--- backend/src/router/trip_router.ts
+++ backend/src/router/trip_router.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import {returnTrips, returnSingleTrip, createTrip, updateTrip, deleteTrip, retrieveAllStartLocations} from '../controller/trip_controller';
+import {returnTrips, returnSingleTrip, createTrip, updateTrip, deleteTrip, retrieveAllStartLocations, retrieveTripStatus} from '../controller/trip_controller';
 const router = express.Router();
 
 // return trip info
 router.get('/', returnTrips);
 router.get('/start-locations', retrieveAllStartLocations);
 router.get('/:id', returnSingleTrip);
+router.get('/status/:id', retrieveTripStatus);
 
 // create, update, delete trip
 router.post('/', createTrip);
