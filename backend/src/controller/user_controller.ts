d7c85acc9c734e5156bf007dd7df049d46610def
George Zarokanellos <geo.zarak@gmail.com>
2024-11-08 16:45:16 +0200

    Now the driver specifies the available seats in his car instead of providing the user's names that are already in the trip without using the app

--- backend/src/controller/user_controller.ts
+++ backend/src/controller/user_controller.ts
@@ -7,6 +7,7 @@ import sequelize from '../database/connect_to_db';
 import { updatedUserInterface } from "../interface/interface";
 import path from "path";
 import fs from "fs";
+import Vehicle from "../model/vehicle";
 
 export const updateUser = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     await sequelize.transaction(async (transaction: Transaction) => {
@@ -241,15 +242,23 @@ export const retrieveUserInfo = async (req: Request, res: Response, next: NextFu
         if(userData){
             let responseData = userData.toJSON();
             let driverData;
+            let vehicleData;
             if(userData.role === 'driver'){
                 driverData = await Driver.findByPk(userId, {
                     attributes: ['nextScheduledTripId']
                 });
+                vehicleData = await Vehicle.findOne({
+                    where: {
+                        ownerId: userId
+                    },
+                    attributes: ['noOfSeats']
+                })
             }
             if(driverData){
                 responseData = {
                     ...responseData,
-                    ...driverData.toJSON()
+                    ...driverData.toJSON(),
+                    ...vehicleData?.toJSON()
                 }
             }
             res.status(200).json(responseData);
