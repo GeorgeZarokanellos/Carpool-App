5e90f37e9bae6c15889e4c3f165543add6ec3197
George Zarokanellos <geo.zarak@gmail.com>
2024-11-09 20:38:11 +0200

    Added side menu that includes instructions on how to use the app for passengers and a logout button to exit the app

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Route} from 'react-router-dom';
-import { IonRouterOutlet, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonBadge} from '@ionic/react';
+import { IonRouterOutlet, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonBadge, IonPage, IonMenu, IonContent, IonButton, IonMenuToggle, IonAlert} from '@ionic/react';
 import { map, chatbox, notifications, person, search } from 'ionicons/icons';
 import Tab2 from './Tab2';
 import SearchTrips from './SearchTrips';
@@ -11,6 +11,9 @@ import { NotificationPage } from './NotificationPage';
 import { DetailedTripInformationPage } from './DetailedTripInformationPage';
 import instance from '../AxiosConfig';
 import { NotificationInterface } from '../interfacesAndTypes/Interfaces';
+import './Main.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Dropdown } from 'react-bootstrap';
 
 //TODO make the tab turn blue when user clicks on a trip from search trips tab
 
@@ -21,9 +24,12 @@ export const Main: React.FC = () => {
   const [profileRefreshKey, setProfileRefreshKey] = useState(0);
   const [userNotifications, setUserNotifications] = useState<NotificationInterface[]>([]);
   const [notificationsNumber, setNotificationsNumber] = useState<number>(0);
+  const [userLogoutConfirmation, setUserLogoutConfirmation] = useState<boolean>(false);
+  const [selectedInstruction, setSelectedInstruction] = useState<string>('');
 
   const userId = localStorage.getItem('userId');
   const userRole = localStorage.getItem('role');
+  
   let queryParams = new URLSearchParams();
   if(userId !== null && userRole !== null) {
       queryParams = new URLSearchParams({
@@ -32,7 +38,6 @@ export const Main: React.FC = () => {
   }
 
   const retrieveNotifications = async () => {
-
     try {
         await instance.get(`/notifications/${userId}?${queryParams.toString()}`)
         .then(response => {
@@ -47,14 +52,93 @@ export const Main: React.FC = () => {
         console.log("Error retrieving notifications", error);
     }
 
-}
+  }
 
-useEffect(() => {
-    retrieveNotifications();
-}, [notificationRefreshKey]);
+  const handleSelectedInstruction = (instruction: string) => {
+    setSelectedInstruction(instruction);
+  }
+
+  const renderInstructions = () => {
+    switch(selectedInstruction){
+      case 'joinTrip':  
+        return(
+              <ol className='instructions-list'>
+                <li>On the search trips screen. Click on a trip that suits your needs.</li>
+                <li>A screen displaying detailed information about the trip will show up.</li>
+                <li>Click on the request to join button at the bottom.</li>
+                <li>A popup message will arise that will display the available stops that the driver can make.</li>
+                <li>Choose the stop that is closer to you.</li>
+                <li>A join request notification will be sent to the driver and once he/she accepts it you will join the trip!</li>
+              </ol>
+        );
+      case 'leaveTrip':
+        return(
+          <ol className='instructions-list'>
+            <li>Navigate to the current trip screen from the navigation bar in the bottom.</li>
+            <li>If the trip hasn&apos;t started, at the bottom of the the screen you are going to see a leave trip button.</li>
+            <li>Press it and you will be removed from the trip.</li>
+            <li>Now you can join another trip if you wish!</li>
+          </ol>
+        );
+      case 'review':
+        return (
+          <ol className='instructions-list'>
+            <li>Go to the notifications screen after you have finished a trip.</li>
+            <li>You will see a trip participants review notification.</li>
+            <li>At the bottom there&apos;s going to be a section displaying a participant along with selectable stars.</li>
+            <li>Select the amount of stars for this participant, swipe right to see the next one and repeat.</li>
+            <li>Once you&apos;ve finished with all of them, click the submit review button.</li>
+            <li>Your reviews will be submitted and you will receive one point for each one!</li>
+          </ol>
+        );
+      case 'close': 
+        return null;
+      default:
+    }
+  }
+
+  useEffect(() => {
+      retrieveNotifications();
+  }, [notificationRefreshKey]);
 
  return (
-    <IonTabs>
+  <>
+    <IonMenu contentId='main-content'>
+      <IonContent>
+        <IonMenuToggle autoHide={false}>
+          <IonButton >Close Menu</IonButton>
+        </IonMenuToggle>
+        <div className='menu-content' >
+          <Dropdown style={{width: '100%'}}>
+            <Dropdown.Toggle >Instructions</Dropdown.Toggle>
+            <Dropdown.Menu>
+              {
+                userRole === 'passenger' ? (
+                  <>
+                    <Dropdown.Item onClick={() => handleSelectedInstruction('joinTrip')}>Joining a trip</Dropdown.Item>
+                    <Dropdown.Item onClick={() => handleSelectedInstruction('leaveTrip')}>Leaving a trip</Dropdown.Item>
+                    <Dropdown.Item onClick={() => handleSelectedInstruction('review')}>Reviewing users</Dropdown.Item>
+                    <Dropdown.Item onClick={() => handleSelectedInstruction('close')}>Close</Dropdown.Item>     
+                  </>
+                ) : (
+                  <>
+                    <Dropdown.Item onClick={() => handleSelectedInstruction('createTrip')}>Creating a trip</Dropdown.Item>
+                    <Dropdown.Item onClick={() => handleSelectedInstruction('delayTrip')}>Delaying a trip</Dropdown.Item>
+                    <Dropdown.Item onClick={() => handleSelectedInstruction('acceptRejectRequest')}>Accepting/Rejecting a request</Dropdown.Item>
+                    <Dropdown.Item onClick={() => handleSelectedInstruction('review')}>Reviewing users</Dropdown.Item>
+                    <Dropdown.Item onClick={() => handleSelectedInstruction('close')}>Close</Dropdown.Item>     
+                  </>
+                )
+              }
+            </Dropdown.Menu>
+          </Dropdown>
+          {renderInstructions()}
+          <IonButton className='logout-button' onClick={() => setUserLogoutConfirmation(true)}>Logout</IonButton>
+        </div>
+      </IonContent>
+    </IonMenu>
+    <IonPage id='main-content'>
+      <IonTabs>
         <IonRouterOutlet>
           <Route path="/main/current-trip" render={() => <CurrentTripPage refreshKey={currentTripRefreshKey} />} />
           <Route path="/main/tab2" component={Tab2} />
@@ -94,5 +178,30 @@ useEffect(() => {
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
+      <IonAlert 
+        isOpen={userLogoutConfirmation}
+        onDidDismiss={() => setUserLogoutConfirmation(false)}
+        header={'Logout'}
+        message={'Are you sure you want to logout?'}
+        buttons={[  
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            handler: () => {
+              setUserLogoutConfirmation(false);
+            }
+          },
+          {
+            text: 'Logout',
+            handler: () => {
+              localStorage.clear();
+              setUserLogoutConfirmation(false);
+              window.location.href = '/';
+            }
+          }
+        ]}
+      />
+    </IonPage>
+  </>
  );
 }
