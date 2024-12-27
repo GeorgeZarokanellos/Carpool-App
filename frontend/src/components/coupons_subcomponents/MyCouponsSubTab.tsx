acf88dc037e678467e27304a48cf9693845ed359
George Zarokanellos <geo.zarak@gmail.com>
2024-12-27 20:50:30 +0200

    Now the user can see the available and his own coupons, buy new ones and delete the used ones all in the coupons tab

--- frontend/src/components/coupons_subcomponents/MyCouponsSubTab.tsx
+++ frontend/src/components/coupons_subcomponents/MyCouponsSubTab.tsx
@@ -1,9 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+import { UserCoupon } from "../../interfacesAndTypes/Types";
+import { IonButton } from "@ionic/react";
+import { CCard, CCardBody, CCollapse } from "@coreui/react";
+import instance from "../../AxiosConfig";
 
-export const MyCouponsSubTab: React.FC = () => {
+interface MyCouponsTabProps {
+    userCoupons: UserCoupon[];
+    setErrorMessage: (errorMessage: string) => void;
+    setErrorAlert: (errorAlert: boolean) => void;
+    setCouponDeletedAlert: (couponDeleted: boolean) => void;
+    setIsLoading: (isLoading: boolean) => void
+}
+
+export const MyCouponsSubTab: React.FC<MyCouponsTabProps> = ({userCoupons, setErrorMessage, setErrorAlert, setCouponDeletedAlert, setIsLoading}) => {
+
+    const [isExpanded, setIsExpanded] = useState(new Array(userCoupons.length).fill(false));
+    const userId = localStorage.getItem('userId');
+    let userIdInt: number;
+    if(userId)
+        userIdInt = parseInt(userId, 10)
+    else 
+        console.log('User id is null', userId);
+        
+    const toggleExpansion = (index: number) => {
+        setIsExpanded((prevState) => {
+            const newState = [...prevState];
+            newState[index] = !newState[index];
+            return newState;
+        });
+    }
+
+    const handleCouponDelete = async (couponId: number) => {
+        try {
+            setIsLoading(true);
+            const response = await instance.delete(`/user/coupons/remove/${couponId}?userId=${userIdInt}`);
+            if(response.status === 404){
+                setErrorMessage(response.data.message);
+                setErrorAlert(true);
+                setIsLoading(false);
+            } else if (response.status === 200){
+                setIsLoading(false);
+                setCouponDeletedAlert(true);
+            }
+        } catch (error) {
+            console.log(error);
+            setIsLoading(false);
+        }
+    }
+
     return (
         <div>
-
+            {
+                userCoupons.map((userCoupon, index) => {
+                    
+                    return (
+                        <div key={index} className="list-item">
+                            <label>{userCoupon.coupon.title}</label>
+                            <IonButton onClick={() => toggleExpansion(index)}>See Details</IonButton>
+                            <div className="extended-coupon-info">
+                                <CCollapse visible={isExpanded[index]}>
+                                    <CCard>
+                                        <CCardBody>
+                                            <p>Code: {userCoupon.coupon.code}</p>
+                                            <p>Discount: {userCoupon.coupon.discountValue}%</p>
+                                            <p>Description: {userCoupon.coupon.description}</p>
+                                            <IonButton color="danger" onClick={() => handleCouponDelete(userCoupon.couponId)}>Delete Coupon</IonButton>
+                                        </CCardBody>
+                                    </CCard>
+                                </CCollapse>
+                            </div>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
--- frontend/src/components/coupons_subcomponents/ShopSubTab.tsx
+++ frontend/src/components/coupons_subcomponents/ShopSubTab.tsx
@@ -1,9 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
+import { Coupon } from "../../interfacesAndTypes/Types";
+import { CCard, CCardBody, CCollapse } from "@coreui/react";
+// import './ShopSubTab.scss';
+import { IonButton } from "@ionic/react";
+import instance from "../../AxiosConfig";
 
-export const ShopSubTab: React.FC = () => {
+interface ShopTabProps {
+    availableCoupons: Coupon[];
+    setErrorMessage: (errorMessage: string) => void;
+    setErrorAlert: (errorAlert: boolean) => void;
+    setIsLoading: (isLoading: boolean) => void;
+    setCouponPurchasedAlert: (couponPurchasedAlert: boolean) => void;
+    userPoints: number;
+}
+
+export const ShopSubTab: React.FC<ShopTabProps> = ({availableCoupons, setErrorMessage, setErrorAlert, setIsLoading, setCouponPurchasedAlert, userPoints}) => {
+    const [isExpanded, setIsExpanded] = useState(new Array(availableCoupons.length).fill(false));
+    const userId = localStorage.getItem('userId');
+    let userIdInt: number;
+    if(userId !== null)
+        userIdInt = parseInt(userId, 10);
+    else 
+        console.log('userId is null', userId);
+
+    const toggleExpansion = (index: number) => {
+        setIsExpanded((prevState) => {
+            const newState = [...prevState]; //creates a copy of the state of the array
+            newState[index] = !newState[index]; //updates the state at the index by inverting
+            return newState; //updates the state with the updated array
+        });
+    }
+    
+    const handlePurchase = async (userId: number, couponPointsCost: number, couponId: number) => {
+        try {
+            setIsLoading(true);
+            const response = await instance.post(`/user/coupons/purchase/${couponId}`, {
+              userId,
+              couponPointsCost
+            });
+            console.log(response);
+            
+            if(response.status === 400){
+                setErrorMessage(response.data.message);
+                setIsLoading(false);
+                setErrorAlert(true);
+            } else if (response.status === 201){
+                setIsLoading(false);
+                setCouponPurchasedAlert(true);
+            }
+        } catch (error) {
+            setIsLoading(false);
+            console.log(error);
+        }
+      }
+
     return(
         <div>
-            <h2>Here are your coupons!</h2>
+            {
+                availableCoupons.map((coupon, index) => {
+                    return (
+                        <div key={index} className="list-item">
+                            <label>{coupon.title}</label>
+                            <IonButton onClick={() => toggleExpansion(index)}>See Details</IonButton>
+                            <div className="extended-coupon-info">
+                                <CCollapse visible={isExpanded[index]}>
+                                    <CCard>
+                                        <CCardBody>
+                                            <p>Discount: {coupon.discountValue}%</p>
+                                            <p>Description: {coupon.description}</p>
+                                            <p>Cost: {coupon.pointsCost}</p>
+                                            <IonButton 
+                                                color={userPoints < coupon.pointsCost ? 'danger' : 'success'} 
+                                                onClick={() => handlePurchase(userIdInt, coupon.pointsCost, coupon.couponId)}
+                                                disabled={userPoints < coupon.pointsCost}
+                                            >{userPoints < coupon.pointsCost ? 'Not enough points!' : 'Purchase Coupon'}</IonButton>
+                                        </CCardBody>
+                                    </CCard>
+                                </CCollapse>
+                            </div>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
